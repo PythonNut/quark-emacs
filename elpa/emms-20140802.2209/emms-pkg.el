2eb7d537adfcb68269741263a2e0165528087347
PythonNut <PythonNut@users.noreply.github.com>
2014-08-04 03:40:15 +0000

    Daily package.el update

---
+++ elpa/emms-20140802.2209/emms-pkg.el
@@ -0,0 +1,2 @@
+(define-package "emms" "20140802.2209" "The Emacs Multimedia System" 'nil :keywords
+  '("emms" "mp3" "mpeg" "multimedia"))
---
+++ elpa/evil-20140803.930/evil-pkg.el
@@ -0,0 +1,3 @@
+(define-package "evil" "20140803.930" "Extensible Vi layer for Emacs."
+  '((undo-tree "0.6.3")
+    (goto-chg "1.6")))
