8a1e6c857e3c8b820f113321415dd4fc146b4995
PythonNut <PythonNut@users.noreply.github.com>
2014-08-04 19:50:10 -0700

    Added some ediff customizations

--- init.el
+++ init.el
@@ -4381,3 +4381,27 @@ Optionally, pass in string to be \"yanked\" via STRING-IN."
 (autoload 'livescript-mode
   "~/.emacs.d/personal/livescript-mode.el"
   "A major mode for LiveScript" t)
+
+;;; ===================================
+;;; Ediff emacs diff for git/svn/hg/etc
+;;; ===================================
+;; side-by-side diffs
+(setq ediff-split-window-function 'split-window-horizontally)
+(setq ediff-window-setup-function 'ediff-setup-windows-plain)
+
+;; preserve origional window config
+(add-hook 'ediff-load-hook
+  (lambda ()
+    (add-hook 'ediff-before-setup-hook
+      (lambda ()
+        ;; (set-face-foreground 'ediff-current-diff-face-A "#6c71c4")
+        ;; (set-face-foreground 'ediff-current-diff-face-B "#cb4b16")
+        ;; (set-face-foreground 'ediff-current-diff-face-C "#859900")
+
+        (setq ediff-saved-window-configuration (current-window-configuration))))
+    
+    (let ((restore-window-configuration
+            (lambda ()
+              (set-window-configuration ediff-saved-window-configuration))))
+      (add-hook 'ediff-quit-hook restore-window-configuration 'append)
+      (add-hook 'ediff-suspend-hook restore-window-configuration 'append))))
