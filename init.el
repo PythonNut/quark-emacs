85430eef09fb9ce2d8b49ec114e3522b2ae01e6c
PythonNut <PythonNut@users.noreply.github.com>
2014-10-10 20:47:35 +0000

    Minor indentation fix

--- init.el
+++ init.el
@@ -1891,10 +1891,10 @@ to replace the symbol under cursor"
 ;; text mode
 (eval-after-load 'flyspell-mode
   '(progn
-    (when (locate-file "hunspell" exec-path)
-      (setq ispell-program-name "hunspell")
-      (add-to-list 'ispell-extra-args "--sug-mode=ultra"))
-    (set 'flyspell-issue-message-flag nil)))
+     (when (locate-file "hunspell" exec-path)
+       (setq ispell-program-name "hunspell")
+       (add-to-list 'ispell-extra-args "--sug-mode=ultra"))
+     (set 'flyspell-issue-message-flag nil)))
 
 (add-hook 'text-mode-hook 'flyspell-mode)
 (add-hook 'prog-mode-hook 'flyspell-prog-mode)
