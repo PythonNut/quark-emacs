12c8e084325413f25708386465835a674b44d71c
PythonNut <PythonNut@users.noreply.github.com>
2014-10-13 04:14:46 +0000

    Added helm-mode-manager

---
+++ elpa/helm-mode-manager-20140224.1504/helm-mode-manager-autoloads.el
@@ -0,0 +1,40 @@
+;;; helm-mode-manager-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (helm-switch-major-mode helm-disable-minor-mode
+;;;;;;  helm-enable-minor-mode) "helm-mode-manager" "helm-mode-manager.el"
+;;;;;;  (21563 20796 458446 143000))
+;;; Generated autoloads from helm-mode-manager.el
+
+(autoload 'helm-enable-minor-mode "helm-mode-manager" "\
+
+
+\(fn)" t nil)
+
+(autoload 'helm-disable-minor-mode "helm-mode-manager" "\
+
+
+\(fn)" t nil)
+
+(autoload 'helm-switch-major-mode "helm-mode-manager" "\
+
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("helm-mode-manager-pkg.el") (21563 20796
+;;;;;;  500096 796000))
+
+;;;***
+
+(provide 'helm-mode-manager-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; helm-mode-manager-autoloads.el ends here
---
+++ elpa/helm-mode-manager-20140224.1504/helm-mode-manager-pkg.el
@@ -0,0 +1,1 @@
+(define-package "helm-mode-manager" "20140224.1504" "Select and toggle major and minor modes with helm" (quote ((helm "1.5.3"))))
