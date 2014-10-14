78cbb6eace64ea8bc12ef517650eb383437eb6a5
PythonNut <PythonNut@users.noreply.github.com>
2014-10-14 23:22:14 +0000

    Specify more semantic minor modes

--- init.el
+++ init.el
@@ -1791,6 +1791,11 @@ to replace the symbol under cursor"
 ;;; Semantic - language analyses
 ;;; ============================
 (add-hook 'prog-mode-hook 'semantic-mode)
+(eval-after-load 'semantic
+  '(progn
+    (global-semanticdb-minor-mode +1)
+    (global-semantic-idle-scheduler-mode +1)
+    (global-semantic-idle-summary-mode +1)))
 
 ;;; =======================================
 ;;; Line numbers - intelligent line numbers
