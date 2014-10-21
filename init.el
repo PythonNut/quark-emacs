328926b817354ce800e5ec8778ab2c6b13f2fd41
PythonNut <PythonNut@users.noreply.github.com>
2014-10-21 18:45:27 +0000

    Enhance dired support

--- init.el
+++ init.el
@@ -2643,6 +2643,23 @@ or expand the word preceding point. Multiple tabs cycle indentation level."
 
 (global-set-key (kbd "C-c C-c s") 'svn-status)
 
+;;; =============================
+;;; Dired, the emacs file manager
+;;; =============================
+(eval-after-load 'dired+
+  '(progn
+     (define-key dired-mode-map (kbd "<down-mouse-3>") 'strokes-do-stroke)
+     (toggle-diredp-find-file-reuse-dir 1)))
+
+(add-hook 'dired-load-hook
+  '(lambda ()
+     (require 'dired-x)
+     (require 'dired+)))
+
+(defun dired-here ()
+  (interactive)
+  (dired (file-name-directory (buffer-file-name))))
+
 ;;; ==============================
 ;;; Ido - interactively do things
 ;;; ==============================
