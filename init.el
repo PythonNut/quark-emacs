6953d0a60b57507ae9bcb9a97ec223d2637afa29
PythonNut <PythonNut@users.noreply.github.com>
2014-10-22 01:58:22 +0000

    Added org module

--- init.el
+++ init.el
@@ -1619,28 +1619,6 @@ The current directory is assumed to be the project's root otherwise."
     :nick "PythonNut"
     :password nil))
 
-;;; ========
-;;; Org mode
-;;; ========
-(eval-after-load 'org
-  '(progn
-     (setq
-       org-return-follows-link t
-       org-return-indent t
-       org-completion-use-ido t
-       org-descriptive-links t)
-
-     ;; Make windmove work in org-mode:
-     (add-hook 'org-shiftup-final-hook 'windmove-up)
-     (add-hook 'org-shiftleft-final-hook 'windmove-left)
-     (add-hook 'org-shiftdown-final-hook 'windmove-down)
-     (add-hook 'org-shiftright-final-hook 'windmove-right)
-
-     (add-to-list 'org-modules 'org-mouse)))
-
-(add-hook 'org-mode-hook
-  '(lambda ()
-     (yas/minor-mode -1)))
 
 (autoload 'bongo "bongo")
 (autoload 'asy-mode "asy-mode")
