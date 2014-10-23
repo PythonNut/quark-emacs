258e16cbce2fee20096feebd961d732115837f70
PythonNut <PythonNut@users.noreply.github.com>
2014-10-23 22:28:34 +0000

    Update basic modes

--- modules/config-modes.el
+++ modules/config-modes.el
@@ -1,3 +1,12 @@
 (tool-bar-mode -1)
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
+(blink-cursor-mode -1)
+(auto-compression-mode +1)
+(transient-mark-mode +1)
+(delete-selection-mode +1)
+
+;; encryption mode
+(setq epa-file-name-regexp "\\.\\(gpg\\|asc\\)$")
+(epa-file-name-regexp-update)
+(setenv "GPG_AGENT_INFO" nil)
