e6eb6187bc87d1eb0adfd75bbd5f9d0a73f58f58
PythonNut <PythonNut@users.noreply.github.com>
2014-10-23 02:59:41 +0000

    Add ability to auto-install packages

--- init.el
+++ init.el
@@ -1,5 +1,9 @@
+(require 'cl)
+(require 'cl-lib)
+
 (add-to-list 'load-path
 	     (concat user-emacs-directory "modules/"))
 
 (load-library "config-setq.el")
 (load-library "config-package.el")
+(load-library "config-ido.el")
