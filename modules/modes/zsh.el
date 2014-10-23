833029c4a744b4a9a0d51912364eca247123ed01
PythonNut <PythonNut@users.noreply.github.com>
2014-10-23 20:28:11 +0000

    Open zsh files in sh-mode

---
+++ modules/modes/zsh.el
@@ -0,0 +1,8 @@
+;; bind zsh files to sh-mode
+(add-to-list 'auto-mode-alist '("\\.zsh\\'" . sh-mode))
+
+;; bind zsh files to the zsh submode of sh-mode
+(add-hook 'sh-mode-hook
+  '(lambda ()
+    (if (string-match "\\.zsh$" buffer-file-name)
+      (sh-set-shell "zsh"))))
