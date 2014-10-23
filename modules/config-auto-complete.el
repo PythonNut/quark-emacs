72db8babb748b24331b80ca264327e31553104d8
PythonNut <PythonNut@users.noreply.github.com>
2014-10-23 16:38:00 +0000

    Add auto-complete module

---
+++ modules/config-auto-complete.el
@@ -0,0 +1,33 @@
+(require 'auto-complete-config)
+
+
+(eval-after-load 'auto-complete
+  '(progn
+     (global-auto-complete-mode +1)
+     (ac-config-default)
+     (ac-flyspell-workaround)
+     (ac-linum-workaround)
+     (ac-set-trigger-key "C-c <C-tab>")
+     
+     (setq ac-sources
+	   '(ac-source-abbrev
+	     ac-source-yasnippet
+	     ac-source-semantic
+	     ac-source-dictionary
+	     ac-source-filename
+	     ac-source-words-in-buffer
+	     ac-source-words-in-same-mode-buffers))
+
+     (setq ac-auto-start t
+	   ac-auto-show-menu 0.5
+	   ac-show-menu-immediately-on-auto-complete t
+	   ac-ignore-case 'smart
+	   ac-delay 0
+	   ac-dwim t
+	   ac-use-fuzzy t
+	   ac-fuzzy-enable t
+	   ac-dwim-enable t
+	   ac-use-comphist t
+	   popup-use-optimized-column-computation nil)
+
+     (add-to-list 'ac-trigger-commands 'backwards-kill-char)))
