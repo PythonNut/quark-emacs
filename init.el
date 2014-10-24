853b5c17abda68724f6657ea0ca01d6616db8c98
PythonNut <PythonNut@users.noreply.github.com>
2014-10-24 01:26:53 +0000

    Set flycheck faces

--- init.el
+++ init.el
@@ -17,7 +17,9 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
-  )
+  '(flycheck-error ((t (:inherit nil :underline "#dc322f"))))
+  '(flycheck-info ((t (:inherit nil :underline "#268bd2"))))
+  '(flycheck-warning ((t (:inherit nil :underline "#b58900")))))
 
 (load-library "config-setq.el")
 (load-library "config-modes.el")
