1744bf4d3a780aafd7810cd87b3848bdd2d4108e
PythonNut <PythonNut@users.noreply.github.com>
2014-10-24 01:26:16 +0000

    Set fringe background after theme load

--- modules/config-solarized.el
+++ modules/config-solarized.el
@@ -1,2 +1,4 @@
 (require 'solarized-theme)
 (load-theme 'solarized-dark)
+
+(set-face-background 'fringe "#022F3A")
--- modules/config-ui.el
+++ modules/config-ui.el
@@ -1,4 +1,2 @@
 (load-library "config-scroll.el")
 (load-library "config-line-numbers.el")
-
-(set-face-background 'fringe "#022F3A")
