9822b16679e1c09bae536495d772131caaf5f8a9
PythonNut <PythonNut@users.noreply.github.com>
2014-10-25 22:58:09 +0000

    Auto byte-compile

--- modules/modes/emacs-lisp.el
+++ modules/modes/emacs-lisp.el
@@ -1,3 +1,8 @@
 (setq
  lisp-indent-offset 2
  lisp-body-indent 2)
+
+(add-hook 'emacs-lisp-mode-hook
+  '(lambda ()
+     (require 'auto-async-byte-compile)
+     (auto-async-byte-compile-mode +1)))
