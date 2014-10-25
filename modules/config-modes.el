a866b50bc58a03047a3243b0d5bc9784e6cfed49
PythonNut <PythonNut@users.noreply.github.com>
2014-10-25 03:24:22 +0000

    Add minor mode priority modifiers

--- modules/config-modes.el
+++ modules/config-modes.el
@@ -11,3 +11,13 @@
 (setq epa-file-name-regexp "\\.\\(gpg\\|asc\\)$")
 (epa-file-name-regexp-update)
 (setenv "GPG_AGENT_INFO" nil)
+
+(defun raise-minor-mode-map-alist (mode-symbol)
+  "Raise `minor-mode-map-alist' priority of MODE-SYMBOL."
+  (let ((x (assq mode-symbol minor-mode-map-alist)))
+    (and x (setq minor-mode-map-alist (cons x (delq x minor-mode-map-alist))))))
+
+(defun lower-minor-mode-map-alist (mode-symbol)
+  "Lower `minor-mode-map-alist' priority of MODE-SYMBOL."
+  (let ((rel (assq mode-symbol minor-mode-map-alist)))
+    (setq minor-mode-map-alist (append (delete rel minor-mode-map-alist) (list rel)))))
