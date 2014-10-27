06d5e274a9c1c23292e067de56e0242e4b5efe86
PythonNut <PythonNut@users.noreply.github.com>
2014-10-27 03:05:40 +0000

    Set mode-line colors

--- modules/config-solarized.el
+++ modules/config-solarized.el
@@ -2,3 +2,7 @@
 (load-theme 'solarized-dark)
 
 (set-face-background 'fringe "#022F3A")
+(set-face-attribute 'mode-line nil :underline nil)
+
+(set-face-background 'mode-line-inactive "#555753")
+(set-face-foreground 'mode-line-inactive "#eeeeec")
