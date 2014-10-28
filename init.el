214d2307ca7fbc0066aac23fb22187d42da20ec1
PythonNut <PythonNut@users.noreply.github.com>
2014-10-28 17:28:17 +0000

    Fix flycheck faces

--- init.el
+++ init.el
@@ -13,13 +13,11 @@
      (quote
        ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default))))
 (custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(flycheck-error ((((supports :underline (:style wave)) (class color) (min-colors 89)) (:underline (:style wave :color "#dc322f") :inherit unspecified)) (((class color) (min-colors 89)) (:foreground "#990A1B" :background "#FF6E64" :weight bold :underline t))))
- '(flycheck-info ((((supports :underline (:style wave)) (class color) (min-colors 89)) (:underline (:style wave :color "#268bd2") :inherit unspecified)) (((class color) (min-colors 89)) (:foreground "#00629D" :background "#69B7F0" :weight bold :underline t))))
- '(flycheck-warning ((((supports :underline (:style wave)) (class color) (min-colors 89)) (:underline (:style wave :color "#b58900") :inherit unspecified)) (((class color) (min-colors 89)) (:foreground "#7B6000" :background "#DEB542" :weight bold :underline t)))))
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+  )
 
 (load-library "config-setq")
 (load-library "config-modes")
