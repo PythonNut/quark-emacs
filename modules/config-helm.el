945d590c341cf2175d9deeec37e759b416097bd9
PythonNut <PythonNut@users.noreply.github.com>
2014-10-31 04:28:32 +0000

    Untabify helm

--- modules/config-helm.el
+++ modules/config-helm.el
@@ -24,25 +24,25 @@
 
     (setq helm-locate
       `((name . "Locate")
-	 (init . helm-locate-set-command)
-	 (candidates-process . helm-locate-init)
-	 (type . file)
-	 (requires-pattern . 3)
-	 (history . ,'helm-file-name-history)
-	 (keymap . ,helm-generic-files-map)
-	 (help-message . helm-generic-file-help-message)
-	 (candidate-number-limit . 999)
-	 (mode-line . helm-generic-file-mode-line-string)))
+         (init . helm-locate-set-command)
+         (candidates-process . helm-locate-init)
+         (type . file)
+         (requires-pattern . 3)
+         (history . ,'helm-file-name-history)
+         (keymap . ,helm-generic-files-map)
+         (help-message . helm-generic-file-help-message)
+         (candidate-number-limit . 999)
+         (mode-line . helm-generic-file-mode-line-string)))
 
     (defun my-helm-buffers (&rest arg)
       (interactive)
       (helm-other-buffer
-	'(helm-source-buffers-list
-	   helm-source-recentf
-	   helm-source-files-in-current-dir
-	   helm-source-files-in-all-dired
-	   helm-source-buffer-not-found)
-	"*helm-find-files"))
+        '(helm-source-buffers-list
+           helm-source-recentf
+           helm-source-files-in-current-dir
+           helm-source-files-in-all-dired
+           helm-source-buffer-not-found)
+        "*helm-find-files"))
 
     (global-set-key (kbd "C-x C-b") 'my-helm-buffers)
     (global-set-key (kbd "C-x b") 'ido-switch-buffer)
@@ -50,110 +50,110 @@
     (defun my-helm-find-files (&rest arg)
       (interactive)
       (helm-other-buffer
-	'(helm-source-recentf
-	   helm-source-buffers-list
-	   helm-source-files-in-current-dir
-	   helm-source-find-files
-	   helm-source-findutils
-	   helm-source-locate
-	   ;; helm-source-tracker-search
-	   )
-	"*helm-find-files"))
+        '(helm-source-recentf
+           helm-source-buffers-list
+           helm-source-files-in-current-dir
+           helm-source-find-files
+           helm-source-findutils
+           helm-source-locate
+           ;; helm-source-tracker-search
+           )
+        "*helm-find-files"))
 
     (global-set-key (kbd "C-x C-f") 'my-helm-find-files)
 
     (defvar my-helm-source-evaluation-result
       '((name . "Evaluation Result")
-	 (init . (lambda () (require 'edebug)))
-	 (dummy)
-	 (multiline)
-	 (mode-line . "C-RET: nl-and-indent, tab: reindent, C-tab:complete, C-p/n: next/prec-line.")
-	 (filtered-candidate-transformer .
-	   (lambda (candidates source)
-	     (list
-	       (condition-case nil
-		 (with-helm-current-buffer
-		   (pp-to-string
-		     (if edebug-active
-		       (edebug-eval-expression
-			 (read helm-pattern))
-		       (eval (read helm-pattern)))))
-		 (error "")))))
-	 (action . (("Copy result to kill-ring" .
-		      (lambda (candidate)
-			(with-current-buffer helm-buffer
-			  (let ((end (save-excursion
-				       (goto-char (point-max))
-				       (search-backward "\n")
-				       (point))))
-			    (kill-region (point) end)))))
-		     ("copy sexp to kill-ring" .
-		       (lambda (candidate)
-			 (kill-new helm-input)))))))
+         (init . (lambda () (require 'edebug)))
+         (dummy)
+         (multiline)
+         (mode-line . "C-RET: nl-and-indent, tab: reindent, C-tab:complete, C-p/n: next/prec-line.")
+         (filtered-candidate-transformer .
+           (lambda (candidates source)
+             (list
+               (condition-case nil
+                 (with-helm-current-buffer
+                   (pp-to-string
+                     (if edebug-active
+                       (edebug-eval-expression
+                         (read helm-pattern))
+                       (eval (read helm-pattern)))))
+                 (error "")))))
+         (action . (("Copy result to kill-ring" .
+                      (lambda (candidate)
+                        (with-current-buffer helm-buffer
+                          (let ((end (save-excursion
+                                       (goto-char (point-max))
+                                       (search-backward "\n")
+                                       (point))))
+                            (kill-region (point) end)))))
+                     ("copy sexp to kill-ring" .
+                       (lambda (candidate)
+                         (kill-new helm-input)))))))
 
     (defun my-helm-omni (&rest arg)
       (interactive)
       (helm-occur-init-source)
       (unless (fboundp 'helm-source-kill-ring)
-	(require 'helm-ring))
+        (require 'helm-ring))
       (unless (fboundp 'helm-source-lacarte)
-	(require 'helm-misc))
+        (require 'helm-misc))
       (unless (fboundp 'helm-source-semantic)
-	(require 'helm-semantic))
+        (require 'helm-semantic))
       (unless (fboundp 'helm-source-projectile-files-list)
-	(require 'helm-projectile)
-	(unless projectile-global-mode-buffers
-	  (projectile-global-mode +1)))
+        (require 'helm-projectile)
+        (unless projectile-global-mode-buffers
+          (projectile-global-mode +1)))
 
       (let ((bufs (list (buffer-name (current-buffer)))))
-	(helm-attrset 'moccur-buffers bufs helm-source-occur)
-	(helm-set-local-variable 'helm-multi-occur-buffer-list bufs)
-	(helm-set-local-variable
-	  'helm-multi-occur-buffer-tick
-	  (cl-loop for b in bufs
-	    collect (buffer-chars-modified-tick (get-buffer b))))
-	(helm-other-buffer
-	  (append '(helm-source-buffers-list)
+        (helm-attrset 'moccur-buffers bufs helm-source-occur)
+        (helm-set-local-variable 'helm-multi-occur-buffer-list bufs)
+        (helm-set-local-variable
+          'helm-multi-occur-buffer-tick
+          (cl-loop for b in bufs
+            collect (buffer-chars-modified-tick (get-buffer b))))
+        (helm-other-buffer
+          (append '(helm-source-buffers-list)
 
-	    ;; projectile explodes when not in project
-	    (when (ignore-errors (projectile-project-root))
-	      '(helm-source-projectile-recentf-list
-		 helm-source-projectile-files-list
-		 helm-source-projectile-buffers-list))
+            ;; projectile explodes when not in project
+            (when (ignore-errors (projectile-project-root))
+              '(helm-source-projectile-recentf-list
+                 helm-source-projectile-files-list
+                 helm-source-projectile-buffers-list))
 
-	    '( ;; files
-	       helm-source-file-cache
-	       helm-source-recentf
-	       helm-source-files-in-current-dir
-	       helm-source-bookmarks
-	       ;; code search
-	       helm-source-semantic
-	       helm-source-imenu
-	       helm-source-occur
-	       ;; internal
-	       helm-source-kill-ring
-	       helm-source-mark-ring
-	       helm-source-register
-	       ;; code construction
-	       ;; helm-source-regexp
-	       helm-source-lacarte
-	       my-helm-source-evaluation-result
-	       ;; file location
-	       helm-source-findutils
-	       helm-source-locate
-	       ;; helm-source-tracker-search
-	       ;; fallback
-	       ;; helm-source-buffer-not-found
-	       ))
-	  "*helm-omni*")))
+            '( ;; files
+               helm-source-file-cache
+               helm-source-recentf
+               helm-source-files-in-current-dir
+               helm-source-bookmarks
+               ;; code search
+               helm-source-semantic
+               helm-source-imenu
+               helm-source-occur
+               ;; internal
+               helm-source-kill-ring
+               helm-source-mark-ring
+               helm-source-register
+               ;; code construction
+               ;; helm-source-regexp
+               helm-source-lacarte
+               my-helm-source-evaluation-result
+               ;; file location
+               helm-source-findutils
+               helm-source-locate
+               ;; helm-source-tracker-search
+               ;; fallback
+               ;; helm-source-buffer-not-found
+               ))
+          "*helm-omni*")))
 
     (defadvice  evil-paste-pop (around auto-helm-omni activate)
       (if (memq last-command
-	    '(evil-paste-after
-	       evil-paste-before
-	       evil-visual-paste))
-	ad-do-it
-	(call-interactively 'my-helm-omni)))
+            '(evil-paste-after
+               evil-paste-before
+               evil-visual-paste))
+        ad-do-it
+        (call-interactively 'my-helm-omni)))
 
     (global-set-key (kbd "C-c C-o") 'my-helm-omni)
     (define-key evil-normal-state-map (kbd "C-c C-o") 'my-helm-omni)
