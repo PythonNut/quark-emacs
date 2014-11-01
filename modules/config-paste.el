6d48add7e39efa9cbd453b946078c0872350fb67
PythonNut <PythonNut@users.noreply.github.com>
2014-11-01 17:43:46 +0000

    Autoload whole-line-or-region

--- modules/config-paste.el
+++ modules/config-paste.el
@@ -1,5 +1,8 @@
-(require 'whole-line-or-region)
+;; (require 'whole-line-or-region)
 (eval-when-compile (require 'cl))
+
+(autoload 'whole-line-or-region-call-with-region "whole-line-or-region" "")
+(autoload 'whole-line-or-region-call-with-prefix "whole-line-or-region" "")
 
 (cua-mode +1)
 
