0dba2d53abc468261fecffd807c68e4885229591
PythonNut <PythonNut@users.noreply.github.com>
2014-11-01 00:47:07 +0000

    Add debugging functions

--- modules/config-pre-bindings.el
+++ modules/config-pre-bindings.el
@@ -1,1 +1,11 @@
 (key-chord-mode +1)
+
+(defun enable-debugging ()
+  (interactive)
+  (setq debug-on-error t))
+
+(defun disable-debugging ()
+  (interactive)
+  (setq debug-on-error nil))
+
+(defun )
