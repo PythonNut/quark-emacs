cc348b467ea6aa5a5e76a425f8d563676e59adef
PythonNut <PythonNut@users.noreply.github.com>
2014-12-09 17:57:56 +0000

    Move custom into its own file

--- init.el
+++ init.el
@@ -1,17 +1,5 @@
-(custom-set-variables
-  ;; custom-set-variables was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
-  '(custom-safe-themes
-     (quote
-       ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default))))
-(custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
-  )
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
 
 ;; polyfill with-eval-after-load
 (when (version< emacs-version "24.4")
