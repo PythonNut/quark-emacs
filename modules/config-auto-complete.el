6735b19b12c5d3bd4f53627aef7ecf7df4067ab1
PythonNut <PythonNut@users.noreply.github.com>
2014-12-10 04:25:13 +0000

    Enable filename completion

--- modules/config-auto-complete.el
+++ modules/config-auto-complete.el
@@ -3,6 +3,9 @@
 (eval-when-compile (require 'auto-complete))
 (eval-when-compile (require 'auto-complete-config))
 
+(add-hook 'first-change-hook
+  (lambda () (require 'auto-complete-config)))
+
 (with-eval-after-load 'auto-complete
   (diminish 'auto-complete-mode " ⇝")
   (ac-config-default)
@@ -10,14 +13,13 @@
   (ac-linum-workaround)
   (ac-set-trigger-key "C-c <C-tab>")
 
-  ;; (setq ac-sources
-  ;;   '(ac-source-abbrev
-  ;;      ac-source-yasnippet
-  ;;      ac-source-semantic
-  ;;      ac-source-dictionary
-  ;;      ac-source-filename
-  ;;      ac-source-words-in-buffer
-  ;;      ac-source-words-in-same-mode-buffers))
+  (setq-default ac-sources
+    (append
+      '(
+         ac-source-semantic
+         ac-source-filename
+         )
+      ac-sources))
 
   (setq ac-auto-start t
     ac-auto-show-menu 0.5
