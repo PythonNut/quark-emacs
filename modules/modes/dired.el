f4256d9e6c67c488822e73b61529f41483e953d7
PythonNut <PythonNut@users.noreply.github.com>
2014-12-11 17:48:09 +0000

    Add some basic dired customization

---
+++ modules/modes/dired.el
@@ -0,0 +1,10 @@
+(eval-when-compile
+  (progn
+    (require 'ls-lisp)))
+
+(with-eval-after-load 'dired
+  (require 'ls-lisp)
+  (setq ls-lisp-use-insert-directory-program nil
+    ls-lisp-support-shell-wildcards t
+    ls-lisp-dirs-first t
+    ls-lisp-verbosity nil))
