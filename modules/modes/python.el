fe09a7d15ad41bab8ec41591a899aaa40cf0b533
PythonNut <PythonNut@users.noreply.github.com>
2014-12-11 04:10:52 +0000

    Move python-mode-hook out of eval-after-load

--- modules/modes/python.el
+++ modules/modes/python.el
@@ -3,13 +3,13 @@
     (require 'evil)
     (require 'jedi)))
 
-(with-eval-after-load 'python
-  (add-hook 'python-mode-hook
-    (lambda ()
-      (add-to-list 'evil-overriding-maps 'jedi-mode-map)
-      (evil-define-key 'normal jedi-mode-map (kbd "gd") 'jedi:goto-definition)
-      (jedi:setup)))
+(add-hook 'python-mode-hook
+  (lambda ()
+    (add-to-list 'evil-overriding-maps 'jedi-mode-map)
+    (evil-define-key 'normal jedi-mode-map (kbd "gd") 'jedi:goto-definition)
+    (jedi:setup)))
 
+(with-eval-after-load 'python
   (setq
     jedi:use-shortcuts t
     jedi:install-imenu t))
