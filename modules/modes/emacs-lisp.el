1dd0ac7a458a02d26a2bd13ca13bcc9400ee495f
PythonNut <PythonNut@users.noreply.github.com>
2014-12-15 23:09:28 +0000

    Add smarter goto-definition support for emacs lisp

--- modules/modes/emacs-lisp.el
+++ modules/modes/emacs-lisp.el
@@ -1,13 +1,23 @@
-(setq
-  lisp-indent-offset 2
-  lisp-body-indent 2)
+(eval-when-compile (require 'evil))
+
+(defun emacs-lisp-goto-definition ()
+  (interactive)
+  (find-function (function-called-at-point)))
 
 (add-hook 'emacs-lisp-mode-hook
   (lambda ()
+    (setq
+      lisp-indent-offset 2
+      lisp-body-indent 2
+      mode-name "λ")
+
     (aggressive-indent-mode +1)
     (require 'auto-async-byte-compile)
     (auto-async-byte-compile-mode +1)
-    (setq mode-name "λ")))
+
+    (local-set-key (kbd "M-.") 'emacs-lisp-goto-definition)
+    (local-set-key (kbd "M-,") 'evil-jump-backward)
+    (evil-define-key 'normal (current-local-map) "gd" 'emacs-lisp-goto-definition)))
 
 (with-eval-after-load 'aggressive-indent
   (diminish 'aggressive-indent-mode " ⇶"))
