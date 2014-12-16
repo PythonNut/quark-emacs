df0c69a4e307a4ef8bc2d62b47806cbbdc4b9d88
PythonNut <PythonNut@users.noreply.github.com>
2014-12-16 19:00:45 +0000

    Fix adaptive-wrap indentation

--- modules/config-wrap.el
+++ modules/config-wrap.el
@@ -1,10 +1,12 @@
 (require 'adaptive-wrap)
 (add-hook 'visual-line-mode-hook 'adaptive-wrap-prefix-mode)
 (global-visual-line-mode +1)
-(setq line-move-visual t)
+(setq
+  line-move-visual t)
 
 (add-hook 'visual-line-mode-hook
   (lambda ()
+    (setq adaptive-wrap-extra-indent 2)
     (diminish 'visual-line-mode)))
 
 (provide 'config-wrap)
