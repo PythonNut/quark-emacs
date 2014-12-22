e04d55eaba01ed8f32fd40b16d67737cdc9b8c1a
PythonNut <PythonNut@users.noreply.github.com>
2014-12-22 17:16:45 +0000

    Remove debugging commands use toggle-debug-* instead

--- modules/config-modes.el
+++ modules/config-modes.el
@@ -39,14 +39,6 @@
 
 (key-chord-mode +1)
 
-(defun enable-debugging ()
-  (interactive)
-  (setq debug-on-error t))
-
-(defun disable-debugging ()
-  (interactive)
-  (setq debug-on-error nil))
-
 (defun really-kill-emacs ()
   "Like `kill-emacs', but ignores `kill-emacs-hook'."
   (interactive)
