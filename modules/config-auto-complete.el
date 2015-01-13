5fbe707560b7ed66c99e29f0de64723f735fbbec
PythonNut <PythonNut@users.noreply.github.com>
2015-01-13 09:46:29 -0800

    Move company config into with-eval-after-load

--- modules/config-auto-complete.el
+++ modules/config-auto-complete.el
@@ -1,37 +1,36 @@
-(eval-when-compile (progn (require 'company)))
-
-(setq
-  company-idle-delay 0.1
-  company-echo-delay 0
-  company-auto-complete 'company-explicit-action-p
-  company-minimum-prefix-length 2
-  company-show-numbers nil
-  company-tooltip-align-annotations t)
+(eval-when-compile (require 'company))
 
 (global-company-mode +1)
 
-(define-key company-active-map (kbd "<tab>") #'company-complete)
-(set-face-background 'company-tooltip-common-selection
-  (face-background 'company-tooltip-selection))
-(set-face-background 'company-tooltip-common
-  (face-background 'company-tooltip))
-
 (with-eval-after-load 'company
-  (diminish 'company-mode (if (display-graphic-p) " γ" " Co")))
+  (diminish 'company-mode (if (display-graphic-p) " γ" " Co"))
+  (setq
+    company-idle-delay 0.1
+    company-echo-delay 0
+    company-auto-complete 'company-explicit-action-p
+    company-minimum-prefix-length 2
+    company-show-numbers nil
+    company-tooltip-align-annotations t)
 
-(defun company-complete-common-or-complete-full ()
-  (interactive)
-  (when (company-manual-begin)
-    (if (eq last-command #'company-complete-common-or-cycle)
+  (defun company-complete-common-or-complete-full ()
+    (interactive)
+    (when (company-manual-begin)
+      (if (eq last-command #'company-complete-common-or-cycle)
         (let ((company-selection-wrap-around t))
           (call-interactively #'company-complete-selection))
-      (let ((buffer-mod-tick (buffer-chars-modified-tick)))
-        (call-interactively #'company-complete-common)
-        (when (= buffer-mod-tick (buffer-chars-modified-tick))
-          (call-interactively #'company-complete-selection)
-          (call-interactively #'company-complete))))))
+        (let ((buffer-mod-tick (buffer-chars-modified-tick)))
+          (call-interactively #'company-complete-common)
+          (when (= buffer-mod-tick (buffer-chars-modified-tick))
+            (call-interactively #'company-complete-selection)
+            (call-interactively #'company-complete))))))
 
-(define-key company-active-map [tab] #'company-complete-common-or-complete-full)
-(define-key company-active-map (kbd "TAB") #'company-complete-common-or-complete-full)
+  (define-key company-active-map [tab] #'company-complete-common-or-complete-full)
+  (define-key company-active-map (kbd "TAB") #'company-complete-common-or-complete-full)
+
+  (define-key company-active-map (kbd "<tab>") #'company-complete)
+  (set-face-background 'company-tooltip-common-selection
+    (face-background 'company-tooltip-selection))
+  (set-face-background 'company-tooltip-common
+    (face-background 'company-tooltip)))
 
 (provide 'config-auto-complete)
