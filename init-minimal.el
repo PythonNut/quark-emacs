d810840ccba2643f869190610ae98a58c1cc2258
PythonNut <PythonNut@users.noreply.github.com>
2015-01-15 20:58:52 +0000

    Add initial minimal config

---
+++ init-minimal.el
@@ -0,0 +1,25 @@
+;; load custom settings from external file
+(load (setq custom-file (concat user-emacs-directory "custom")))
+
+(icomplete-mode +1)
+(show-paren-mode +1)
+(xterm-mouse-mode +1)
+
+(set-face-attribute 'show-paren-match nil
+  :background nil
+  :weight 'extra-bold)
+
+(eval-and-compile
+  (add-to-list 'load-path
+    (concat user-emacs-directory "modules/")))
+
+(require 'config-setq)
+
+(setq
+  lisp-indent-offset 2
+  lisp-body-indent 2)
+
+(require 'config-safety)
+
+(load-theme 'tango-dark)
+
