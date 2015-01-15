896a24dead18b99668ab215b68d4bafd0d428f93
PythonNut <PythonNut@users.noreply.github.com>
2015-01-14 20:29:38 -0800

    Cleanup compiled yasnippets

--- data/snippets/c-mode/.yas-compiled-snippets.el
+++
@@ -1,9 +0,0 @@
-;;; Compiled snippets and support files for `c-mode'
-;;; Snippet definitions:
-;;;
-(yas-define-snippets 'c-mode
-  '(("fopen" "FILE *${fp} = fopen(${\"file\"}, \"${r}\");" "FILE *fp = fopen(..., ...);" nil nil nil nil nil nil)
-     ("printf" "printf (\"${1:%s}\\\\n\"${1:$(if (string-match \"%\" text) \",\" \"\\);\")\n}$2${1:$(if (string-match \"%\" text) \"\\);\" \"\")}" "printf" nil nil nil nil nil nil)))
-
-
-;;; Do not edit! File generated at Tue May  6 17:54:00 2014
--- data/snippets/cperl-mode/.yas-compiled-snippets.el
+++
@@ -1,2 +0,0 @@
-;;; Compiled snippets and support files for `cperl-mode'
-;;; Do not edit! File generated at Tue May  6 17:54:00 2014
