abbc1b70c8ebd4d9d9cb19aced10bcc6e3dd6205
PythonNut <PythonNut@users.noreply.github.com>
2015-01-18 18:05:40 -0800

    Prefer more modern C standards (like C++11) in flycheck

---
+++ modules/modes/c-like.el
@@ -0,0 +1,9 @@
+;; prefer C++1y
+(add-hook 'c++-mode-hook
+  (lambda ()
+    (setq flycheck-clang-language-standard "c++1y")))
+
+;; prefer C11
+(add-hook 'c-mode-hook
+  (lambda ()
+    (setq flycheck-clang-language-standard "c11")))
