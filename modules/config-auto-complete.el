8a236862c1433e4d77522b82ceb500baf1cc7901
PythonNut <PythonNut@users.noreply.github.com>
2015-01-20 15:37:37 -0800

    Theme company popups
    Currently, the theme is nice looking, but boring
    I may want to fix that in the future

--- modules/config-auto-complete.el
+++ modules/config-auto-complete.el
@@ -33,10 +33,33 @@
   (define-key company-active-map [tab] #'company-complete-common-or-complete-full)
   (define-key company-active-map (kbd "TAB") #'company-complete-common-or-complete-full)
 
-  (set-face-background 'company-tooltip-common-selection
-    (face-background 'company-tooltip-selection))
-  (set-face-background 'company-tooltip-common
-    (face-background 'company-tooltip)))
+  (add-hook 'load-theme-hook
+    (lambda ()
+      (set-face-attribute 'company-tooltip-common-selection nil
+        :background "#839496"
+        :foreground nil
+        :underline nil
+        :inherit 'region)
+
+      (set-face-attribute 'company-tooltip-selection nil
+        :background "#586e75"
+        :foreground nil
+        :inherit 'region)
+
+      (set-face-attribute 'company-tooltip-common nil
+        :background nil
+        :underline nil
+        :inherit 'company-tooltip
+        :foreground "#586e75")
+
+      (set-face-attribute 'company-tooltip-annotation nil
+        :foreground nil
+        :background nil
+        :inherit 'company-tooltip)
+
+      (set-face-attribute 'company-tooltip nil
+        :foreground nil
+        :inherit 'default))))
 
 (with-eval-after-load 'company-dabbrev-code
   (setq company-dabbrev-code-everywhere t))
