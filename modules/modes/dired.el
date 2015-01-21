dbd9922ffd126246cb245a130cdd851c0473b47e
PythonNut <PythonNut@users.noreply.github.com>
2015-01-21 09:41:54 -0800

    Minor dired keybinding tweaks

--- modules/modes/dired.el
+++ modules/modes/dired.el
@@ -15,19 +15,38 @@
       (dired-up-directory)
       (kill-buffer old)))
 
+  (defun my-dired-enable-wdired ()
+    (interactive)
+    (unless (evil-insert-state-p)
+      (evil-insert-state))
+    (wdired-change-to-wdired-mode))
+
   (evil-define-key 'normal dired-mode-map "h" #'my-dired-up-directory)
   (evil-define-key 'normal dired-mode-map "l" #'dired-find-alternate-file)
+  (evil-define-key 'normal dired-mode-map "j" #'dired-next-line)
+  (evil-define-key 'normal dired-mode-map "k" #'dired-previous-line)
+
+  (evil-define-key 'normal dired-mode-map "I" #'my-dired-enable-wdired)
+
   (evil-define-key 'normal dired-mode-map "o" #'dired-sort-toggle-or-edit)
-  (evil-define-key 'normal dired-mode-map "v" #'dired-toggle-marks)
-  (evil-define-key 'normal dired-mode-map "m" #'dired-mark)
-  (evil-define-key 'normal dired-mode-map "u" #'dired-unmark)
-  (evil-define-key 'normal dired-mode-map "U" #'dired-unmark-all-marks)
+  (evil-define-key 'normal dired-mode-map "m" #'dired-toggle-marks)
+  (evil-define-key 'normal dired-mode-map "v" #'dired-mark)
+  (evil-define-key 'normal dired-mode-map "V" #'dired-unmark)
+  (evil-define-key 'normal dired-mode-map (kbd "C-v") #'dired-unmark-all-marks)
+  (evil-define-key 'normal dired-mode-map "u" #'dired-undo)
   (evil-define-key 'normal dired-mode-map "c" #'dired-create-directory)
+
   (evil-define-key 'normal dired-mode-map "n" #'evil-search-next)
   (evil-define-key 'normal dired-mode-map "N" #'evil-search-previous)
   (evil-define-key 'normal dired-mode-map "q" #'kill-this-buffer)
 
-  (setq ls-lisp-use-insert-directory-program nil
+  (require 'dired-avfs)
+
+  (require 'dired-filter)
+  (evil-define-key 'normal dired-mode-map "|" dired-filter-map)
+
+  (setq
+    ls-lisp-use-insert-directory-program nil
     ls-lisp-support-shell-wildcards t
     ls-lisp-dirs-first t
     ls-lisp-verbosity nil))
