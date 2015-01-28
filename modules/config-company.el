fcf480f741b1398b1c7efe28f1a497ca6e0f9489
PythonNut <PythonNut@users.noreply.github.com>
2015-01-27 18:31:21 -0800

    Refactor company faces settings

--- modules/config-company.el
+++ modules/config-company.el
@@ -77,41 +77,46 @@
   (define-key company-active-map (kbd "TAB")
     #'company-complete-common-or-complete-full)
 
-  (add-hook 'load-theme-hook
-    (lambda ()
-      (set-face-attribute 'company-tooltip-common-selection nil
-        :background "#839496"
-        :foreground nil
-        :underline nil
-        :inherit 'region)
+  (defun setup-company-tooltip-faces  ()
+    (set-face-attribute 'company-tooltip-common-selection nil
+      :background "#839496"
+      :foreground (if (< (display-color-cells) 256)
+                    "black"
+                    nil)
+      :underline nil
+      :inherit 'region)
 
-      (set-face-attribute 'company-tooltip-selection nil
-        :background "#586e75"
-        :foreground nil
-        :inherit 'region)
+    (set-face-attribute 'company-tooltip-selection nil
+      :background "#586e75"
+      :foreground nil
+      :inherit 'region)
 
-      (set-face-attribute 'company-tooltip-common nil
-        :background nil
-        :underline nil
-        :inherit 'company-tooltip
-        :foreground "#586e75")
+    (set-face-attribute 'company-tooltip-common nil
+      :background nil
+      :underline nil
+      :inherit 'company-tooltip
+      :foreground "#586e75")
 
-      (set-face-attribute 'company-tooltip-annotation nil
-        :foreground nil
-        :background nil
-        :inherit 'company-tooltip)
+    (set-face-attribute 'company-tooltip-annotation nil
+      :foreground nil
+      :background nil
+      :inherit 'company-tooltip)
 
-      (set-face-attribute 'company-tooltip nil
-        :foreground nil
-        :inherit 'default))))
+    (set-face-attribute 'company-tooltip nil
+      :foreground nil
+      :inherit 'default))
+
+  (add-hook 'load-theme-hook #'setup-company-tooltip-faces)
+  (setup-company-tooltip-faces))
 
 (with-eval-after-load 'company-template
-  (add-hook 'load-theme-hook
-    (lambda ()
-      (set-face-attribute 'company-template-field nil
-        :foreground nil
-        :background nil
-        :inherit 'region))))
+  (defun setup-company-template-faces ()
+    (set-face-attribute 'company-template-field nil
+      :foreground nil
+      :background nil
+      :inherit 'region))
+  (add-hook 'load-theme-hook #'setup-company-template-faces)
+  (setup-company-template-faces))
 
 (with-eval-after-load 'company-dabbrev-code
   (setq company-dabbrev-code-everywhere t))
