13e53359ad49826c64f2de022d89ad45d8986bdc
PythonNut <PythonNut@users.noreply.github.com>
2015-02-09 12:02:26 -0800

    1 ⇒ t (when applicable)

--- modules/config-setq.el
+++ modules/config-setq.el
@@ -3,10 +3,10 @@
   cursor-type 'box
   delete-by-moving-to-trash t
   echo-keystrokes 0.4
-  focus-follows-mouse 1
+  focus-follows-mouse t
   gc-cons-threshold 20000000
   indent-tabs-mode nil
-  inhibit-default-init 1
+  inhibit-default-init t
   inhibit-startup-echo-area-message t
   inhibit-startup-screen t
   interprogram-cut-function #'x-select-text
