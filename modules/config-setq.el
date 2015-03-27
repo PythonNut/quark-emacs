bbf89b4970547e2a8b728f5045c5900994f128aa
PythonNut <PythonNut@users.noreply.github.com>
2015-03-27 03:29:57 +0000

    Make clipboard settings more sane

--- modules/config-setq.el
+++ modules/config-setq.el
@@ -23,13 +23,10 @@
   mouse-drag-copy-region nil
   right-margin-width 0
   ring-bell-function 'ignore
-  select-active-regions t
   split-height-threshold 0
   split-width-threshold 0
   tab-width 4
   use-dialog-box nil
-  x-select-enable-clipboard t
-  x-select-enable-primary t
   x-select-request-type '(UTF8_STRING COMPOUND_TEXT TEXT STRING)
   x-stretch-cursor t)
 
