4302815151b5815c26627a39f82e3e3a2acebed1
PythonNut <PythonNut@users.noreply.github.com>
2015-04-06 04:16:03 +0000

    Add pop-window-into-frame

--- modules/config-ui.el
+++ modules/config-ui.el
@@ -103,6 +103,13 @@
 
 (global-set-key (kbd "C-.") #'er/expand-region)
 
+(defun pop-window-into-frame ()
+  (interactive)
+  (let ((buffer (current-buffer)))
+    (unless (one-window-p)
+      (delete-window))
+    (display-buffer-pop-up-frame buffer nil)))
+
 ;;; ====================================
 ;;; iflib - switch buffers alt-tab style
 ;;; ====================================
