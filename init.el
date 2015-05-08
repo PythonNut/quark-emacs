3122738d741699c859252ad76409213b18fea977
PythonNut <PythonNut@users.noreply.github.com>
2015-05-08 15:18:20 +0000

    Disable package-initialize in init.el

--- init.el
+++ init.el
@@ -1,7 +1,7 @@
 ;; Added by Package.el.  This must come before configurations of
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
-(package-initialize)
+;;(package-initialize)
 
 (setq load-prefer-newer t)
 
