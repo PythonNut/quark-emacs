48a3a0269e920f13778e4dd41bb06b0b954e0b7e
PythonNut <PythonNut@users.noreply.github.com>
2015-05-20 17:12:41 +0000

    Simplify python-mode hooks

--- modules/modes/python.el
+++ modules/modes/python.el
@@ -5,18 +5,17 @@
     (require 'anaconda-mode)
     (require 'company-anaconda)))
 
+(add-hook 'python-mode-hook 'anaconda-mode)
+(add-hook 'python-mode-hook 'eldoc-mode)
+
 (add-hook 'python-mode-hook
   (lambda ()
-    (anaconda-mode +1)
-    (eldoc-mode +1)
-    (global-semantic-idle-summary-mode -1)
-
-    (run-at-time 1 nil #'python-indent-guess-indent-offset)))
+    ;; conflicts with `eldoc-mode'
+    (semantic-idle-summary-mode -1)))
 
 (with-eval-after-load 'python
-  (setq python-indent-guess-indent-offset t)
   (evil-define-key 'normal python-mode-map "gd" #'anaconda-mode-goto)
   (define-key python-mode-map (kbd "M-.") #'anaconda-mode-goto))
 
-(with-eval-after-load 'anaconda-mode
-  (diminish 'anaconda-mode " ✶"))
+  (with-eval-after-load 'anaconda-mode
+    (diminish 'anaconda-mode " ✶"))
