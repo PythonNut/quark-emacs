2d32c233b4b319c0ee67a8c846b56854c67de8d1
PythonNut <PythonNut@users.noreply.github.com>
2015-06-13 01:22:15 +0000

    Remove references to ac-js2

--- modules/modes/js.el
+++ modules/modes/js.el
@@ -1,12 +1,9 @@
 (eval-when-compile
   (with-demoted-errors
     (require 'js2-mode)
-    (require 'js2-refactor)
-    (require 'ac-js2)))
+    (require 'js2-refactor)))
 
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
-
-(add-hook 'js2-mode-hook #'ac-js2-mode)
 
 (with-eval-after-load 'js2-mode
   (set-face-foreground 'js2-external-variable
@@ -15,6 +12,5 @@
   (set-face-attribute 'js2-external-variable nil :underline t)
   (js2r-add-keybindings-with-prefix "C-c C-r")
   (setq
-    ac-js2-evaluate-calls t
     js2-basic-offset 2
     js2-bounce-indent-p t))
