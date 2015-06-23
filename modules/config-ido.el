b63106f8686224bb419bfa43d976154c071c9730
PythonNut <PythonNut@users.noreply.github.com>
2015-06-23 15:51:02 +0000

    Remove fixes from DarwinAwardWinner/ido-ubiquitous#84

--- modules/config-ido.el
+++ modules/config-ido.el
@@ -7,8 +7,6 @@
     (require 'ido-ubiquitous)
     (require 'smex)))
 
-(defvar ido-context-switch-command nil)
-
 (setq
   ido-enable-flex-matching t
   ido-save-directory-list-file (concat user-emacs-directory "ido.last")
