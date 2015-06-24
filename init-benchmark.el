7044eb9b31c8621fc2c2294fc138aff84e5fb09e
PythonNut <PythonNut@users.noreply.github.com>
2015-06-24 18:02:29 +0000

    Resynchronize init-benchmark

--- init-benchmark.el
+++ init-benchmark.el
@@ -1,25 +1,64 @@
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;;(package-initialize)
+
 (setq load-prefer-newer t)
+
 (eval-when-compile (require 'cl-lib))
 
 (eval-and-compile
   (add-to-list 'load-path
-    (concat user-emacs-directory "modules/")))
+    (concat user-emacs-directory "modules/"))
+
+  (defmacro when* (condition &rest body)
+    (when condition
+      `(progn ,@body))))
+
+;; polyfill with-eval-after-load
+(when* (version< emacs-version "24.4")
+  (defmacro with-eval-after-load (thing &rest sexps)
+    `(eval-after-load ,thing '(progn ,@sexps))))
+
+;; suppress the GNU spam
+(add-hook 'emacs-startup-hook (lambda () (message "")))
+
+(defadvice load (before quiet-loading
+                  (FILE &optional NOERROR NOMESSAGE NOSUFFIX MUST-SUFFIX)
+                  activate preactivate compile)
+  (setq NOMESSAGE t))
 
 (load (setq custom-file (concat user-emacs-directory "custom.el")))
-(load-library "config-setq")
-(load-library "config-package")
-(load-library "config-modes")
-(load-library "config-desktop")
-(load-library "config-safety")
-(load-library "config-evil")
-(load-library "config-ui")
-(load-library "config-whitespace")
-(load-library "config-paste")
-(load-library "config-company")
-(load-library "config-vcs")
-(load-library "config-minibuffer")
-(load-library "config-intel")
-(load-library "config-solarized")
+
+(message "[              ]")
+(require 'config-setq)
+(message "[=             ]")
+(require 'config-package)
+(message "[==            ]")
+(require 'config-modes)
+(message "[===           ]")
+(require 'config-desktop)
+(message "[====          ]")
+(require 'config-safety)
+(message "[=====         ]")
+(require 'config-evil)
+(message "[======        ]")
+(require 'config-ui)
+(message "[=======       ]")
+(require 'config-whitespace)
+(message "[========      ]")
+(require 'config-paste)
+(message "[=========     ]")
+(require 'config-company)
+(message "[==========    ]")
+(require 'config-vcs)
+(message "[===========   ]")
+(require 'config-minibuffer)
+(message "[============  ]")
+(require 'config-intel)
+(message "[============= ]")
+(require 'config-solarized)
+(message "[==============]")
 
 (eval-when-compile
   (ignore-errors
@@ -30,3 +69,6 @@
   load-dirs (concat
               user-emacs-directory
               "modules/modes/"))
+
+(ad-disable-advice 'load 'before 'quiet-loading)
+(ad-activate 'load)
