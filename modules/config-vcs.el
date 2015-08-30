c7cbc3acb272970004f77c61ce398abbf0f46ea0
PythonNut <PythonNut@users.noreply.github.com>
2015-08-30 22:55:25 +0000

    Add a projectile hydra

--- modules/config-vcs.el
+++ modules/config-vcs.el
@@ -215,55 +215,73 @@ This requires the external program `diff' to be in your `exec-path'."
   (projectile-global-mode +1)
   (require 'magit)
   (setq projectile-mode-line
-    '(:eval (format (if (display-graphic-p) " ↠" " pro")))))
+    '(:eval (format (if (display-graphic-p) " ↠" " pro"))))
+  (define-key projectile-mode-map (kbd "C-c p") #'my/smart-projectile-tools))
 
-(eval-and-compile
-  (cl-macrolet
-    ((define-temp-projectile-binding (key func)
-       `(progn
-          (autoload ,func "projectile")
-          (global-set-key ,(kbd (concat "C-c p " key)) ,func))))
+(defun my/smart-projectile-tools ()
+  (interactive)
+  (unless (fboundp 'hydra/projectile-tools/body)
+    (require 'hydra)
+    (defhydra hydra/projectile-tools (:color blue :hint nil :idle 0.3)
+      "
+Find^^             Operate on project^^      Other window
+_f_ file           _c_ compile project       _O f_ file
+_d_ dir            _R_ regenerate tags       _O d_ dir
+_g_ file dwim      _S_ save project          _O g_ file dwim
+_a_ other file     ^!^ command in /          _O a_ other file
+_l_ file in dir    ^&^ async command in /    _O b_ switch buffer
+_T_ test file      _z_ cache current file    _O t_ implementation←→test
+_j_ tag            _i_ invalidate cache      _O C-o_ display buffer
 
-    (with-no-warnings
-      (generate-calls define-temp-projectile-binding
-        (
-          ("4 a" #'projectile-find-other-file-other-window)
-          ("4 b" #'projectile-switch-to-buffer-other-window)
-          ("4 C-o" #'projectile-display-buffer)
-          ("4 d" #'projectile-find-dir-other-window)
-          ("4 f" #'projectile-find-file-other-window)
-          ("4 g" #'projectile-find-file-dwim-other-window)
-          ("4 t" #'projectile-find-implementation-or-test-other-window)
-          ("!" #'projectile-run-shell-command-in-root)
-          ("&" #'projectile-run-async-shell-command-in-root)
-          ("a" #'projectile-find-other-file)
-          ("b" #'projectile-switch-to-buffer)
-          ("c" #'projectile-compile-project)
-          ("d" #'projectile-find-dir)
-          ("D" #'projectile-dired)
-          ("e" #'projectile-recentf)
-          ("f" #'projectile-find-file)
-          ("g" #'projectile-find-file-dwim)
-          ("F" #'projectile-find-file-in-known-projects)
-          ("i" #'projectile-invalidate-cache)
-          ("I" #'projectile-ibuffer)
-          ("j" #'projectile-find-tag)
-          ("k" #'projectile-kill-buffers)
-          ("l" #'projectile-find-file-in-directory)
-          ("m" #'projectile-commander)
-          ("o" #'projectile-multi-occur)
-          ("p" #'projectile-switch-project)
-          ("P" #'projectile-test-project)
-          ("r" #'projectile-replace)
-          ("R" #'projectile-regenerate-tags)
-          ("s a" #'helm-projectile-ack)
-          ("s g" #'projectile-grep)
-          ("s s" #'helm-projectile-ag)
-          ("S" #'projectile-save-project-buffers)
-          ("t" #'projectile-toggle-between-implementation-and-test)
-          ("T" #'projectile-find-test-file)
-          ("v" #'projectile-vc)
-          ("z" #'projectile-cache-current-file)
-          ("ESC" #'projectile-project-buffers-other-buffer))))))
+Special^^       Buffers^^                    Search and replace
+_D_ dired       _b_ switch buffer            _o_ multi-occur
+_e_ recentf     _k_ kill buffers             _r_ replace
+_I_ ibuffer     _p_ switch project           _s g_ grep
+_v_ vc          _ESC_ project other buffer   _s s_ ag
+_m_ commander   _F_ file any project         _s a_ ack
+
+Tests   _P_ test-project    _t_ toggle implementation←→test"
+      ("O a" projectile-find-other-file-other-window)
+      ("O b" projectile-switch-to-buffer-other-window)
+      ("O C-o" projectile-display-buffer)
+      ("O d" projectile-find-dir-other-window)
+      ("O f" projectile-find-file-other-window)
+      ("O g" projectile-find-file-dwim-other-window)
+      ("O t" projectile-find-implementation-or-test-other-window)
+      ("!" projectile-run-shell-command-in-root)
+      ("&" projectile-run-async-shell-command-in-root)
+      ("a" projectile-find-other-file)
+      ("b" projectile-switch-to-buffer)
+      ("c" projectile-compile-project)
+      ("d" projectile-find-dir)
+      ("D" projectile-dired)
+      ("e" projectile-recentf)
+      ("f" projectile-find-file)
+      ("g" projectile-find-file-dwim)
+      ("F" projectile-find-file-in-known-projects)
+      ("i" projectile-invalidate-cache)
+      ("I" projectile-ibuffer)
+      ("j" projectile-find-tag)
+      ("k" projectile-kill-buffers)
+      ("l" projectile-find-file-in-directory)
+      ("m" projectile-commander)
+      ("o" projectile-multi-occur)
+      ("p" projectile-switch-project)
+      ("P" projectile-test-project)
+      ("r" projectile-replace)
+      ("R" projectile-regenerate-tags)
+      ("s a" helm-projectile-ack)
+      ("s g" projectile-grep)
+      ("s s" helm-projectile-ag)
+      ("S" projectile-save-project-buffers)
+      ("t" projectile-toggle-between-implementation-and-test)
+      ("T" projectile-find-test-file)
+      ("v" projectile-vc)
+      ("z" projectile-cache-current-file)
+      ("ESC" projectile-project-buffers-other-buffer)))
+
+  (hydra/projectile-tools/body))
+
+(global-set-key (kbd "C-c p") #'my/smart-projectile-tools)
 
 (provide 'config-vcs)
