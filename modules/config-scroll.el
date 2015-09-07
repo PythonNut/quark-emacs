a6d5a15cfbbd0ce13b8206a809cc40f26e1abf40
PythonNut <PythonNut@users.noreply.github.com>
2015-09-07 17:01:24 +0000

    Stop setting scroll-margin manually

--- modules/config-scroll.el
+++ modules/config-scroll.el
@@ -4,15 +4,9 @@
       mouse-wheel-progressive-speed nil
       mouse-wheel-follow-mouse t
       smooth-scroll-margin 5
-      scroll-margin 5
       scroll-step 1
       auto-window-vscroll nil
       scroll-conservatively 1000)
-
-;; scroll-margin is reset at start for some reason
-(add-hook 'emacs-startup-hook
-          (lambda ()
-            (setq scroll-margin 5)))
 
 (global-set-key (kbd "<left-margin> <mouse-5>")
                 (kbd "<mouse-5> <mouse-5> <mouse-5>"))
