59e1c9ac2601895eb73c966383be5234279d6a70
PythonNut <PythonNut@users.noreply.github.com>
2015-09-07 04:23:02 +0000

    Refactor and expand restart options

--- modules/config-setq.el
+++ modules/config-setq.el
@@ -1,41 +1,39 @@
 ;; -*- lexical-binding: t -*-
 
-(setq
- ad-redefinition-action 'accept
- cursor-type 'box
- delete-by-moving-to-trash t
- echo-keystrokes 0.4
- focus-follows-mouse t
- gc-cons-threshold 20000000
- indent-tabs-mode nil
- inhibit-default-init t
- inhibit-startup-echo-area-message t
- inhibit-startup-screen t
- inhibit-x-resources t
- interprogram-cut-function #'x-select-text
- interprogram-paste-function #'x-selection-value
- jit-lock-defer-time 0.04
- jit-lock-stealth-nice 0.1
- jit-lock-stealth-time 0.2
- jit-lock-stealth-verbose nil
- left-margin-width 0
- max-lisp-eval-depth 5000
- max-specpdl-size 10000
- mode-line-end-spaces nil
- mouse-autoselect-window 0.3
- mouse-drag-copy-region nil
- right-margin-width 0
- ring-bell-function 'ignore
- split-height-threshold 0
- split-width-threshold 0
- tab-width 4
- use-dialog-box nil
- x-select-request-type '(UTF8_STRING COMPOUND_TEXT TEXT STRING)
- x-stretch-cursor t)
+(setq ad-redefinition-action 'accept
+      cursor-type 'box
+      delete-by-moving-to-trash t
+      echo-keystrokes 0.4
+      focus-follows-mouse t
+      gc-cons-threshold 20000000
+      indent-tabs-mode nil
+      inhibit-default-init t
+      inhibit-startup-echo-area-message t
+      inhibit-startup-screen t
+      inhibit-x-resources t
+      interprogram-cut-function #'x-select-text
+      interprogram-paste-function #'x-selection-value
+      jit-lock-defer-time 0.04
+      jit-lock-stealth-nice 0.1
+      jit-lock-stealth-time 0.2
+      jit-lock-stealth-verbose nil
+      left-margin-width 0
+      max-lisp-eval-depth 5000
+      max-specpdl-size 10000
+      mode-line-end-spaces nil
+      mouse-autoselect-window 0.3
+      mouse-drag-copy-region nil
+      right-margin-width 0
+      ring-bell-function 'ignore
+      split-height-threshold 0
+      split-width-threshold 0
+      tab-width 4
+      use-dialog-box nil
+      x-select-request-type '(UTF8_STRING COMPOUND_TEXT TEXT STRING)
+      x-stretch-cursor t)
 
-(setq-default
- buffer-file-coding-system 'utf-8
- indent-tabs-mode nil)
+(setq-default buffer-file-coding-system 'utf-8
+              indent-tabs-mode nil)
 
 (put 'set-goal-column 'disabled nil)
 (put 'narrow-to-region 'disabled nil)
@@ -49,27 +47,41 @@
 
 (defalias 'yes-or-no-p #'y-or-n-p)
 
+(defun my/restart-emacs-engine (&rest args)
+  (let ((command-args (mapconcat (lambda (x) (format "%s" x))
+                                 args
+                                 " ")))
+    (add-hook 'kill-emacs-hook
+              (lambda ()
+                (if (display-graphic-p)
+                    (call-process "sh" nil nil nil "-c"
+                                  (format "emacs %s &"
+                                          command-args))
+                  (suspend-emacs (format "(emacs %s -nw < `tty`) & fg; fg"
+                                         command-args))))
+              t)
+    (save-buffers-kill-emacs)))
+
+(defun restart-emacs (&optional arg)
+  (interactive "P")
+  (cond
+   ((or (consp arg) (and (numberp arg) (> arg 0)))
+    (desktop-save-in-desktop-dir)
+    (my/restart-emacs-engine "--eval '(desktop-read)'"))
+   ((eq '- arg)
+    (my/restart-emacs-engine "--debug-init"))
+   ((and (numberp arg) (< arg 0))
+    (my/restart-emacs-engine "--eval '(desktop-read)' --debug-init"))
+   (t
+    (my/restart-emacs-engine))))
+
 (defun restart-emacs (&optional arg)
   (interactive "p")
   ;; We need the new emacs to be spawned after all kill-emacs-hooks
   ;; have been processed and there is nothing interesting left
-  (if (and (called-interactively-p 'any) (/= arg 4))
-      (add-hook 'kill-emacs-hook
-                (lambda ()
-                  (if (display-graphic-p)
-                      (call-process "sh" nil nil nil "-c" "emacs &")
-                    (suspend-emacs "(emacs -nw < `tty`) & fg; fg")))
-                t)
+  (if (and (integerp arg) (/= arg 4))
 
-    (desktop-save-in-desktop-dir)
-    (add-hook 'kill-emacs-hook
-              (lambda ()
-                (if (display-graphic-p)
-                    (call-process "sh" nil nil nil "-c" "emacs --eval '(desktop-read)'&")
-                  (suspend-emacs
-                   "(emacs --eval '(desktop-read)' -nw < `tty`) & fg; fg")))
-              t))
-  (save-buffers-kill-emacs))
+      ))
 
 (defun byte-recompile-config ()
   (interactive)
