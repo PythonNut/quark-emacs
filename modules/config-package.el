120353f4f65e22a5c47eed2c87957b86f005145f
PythonNut <PythonNut@users.noreply.github.com>
2015-09-07 16:34:56 +0000

    Add function to upgrade all packages

--- modules/config-package.el
+++ modules/config-package.el
@@ -160,4 +160,35 @@
         (with-demoted-errors "Idle load error: %s"
           (require sym nil t))))))
 
+(defun package-upgrade-all (&optional automatic)
+  "Upgrade all packages automatically without showing *Packages* buffer."
+  (interactive)
+  (package-refresh-contents)
+  (let (upgrades)
+    (cl-flet ((get-version (name where)
+                           (let ((pkg (cadr (assq name where))))
+                             (when pkg
+                               (package-desc-version pkg)))))
+      (dolist (package (mapcar #'car package-alist))
+        (let ((in-archive (get-version package package-archive-contents)))
+          (when (and in-archive
+                     (version-list-< (get-version package package-alist)
+                                     in-archive))
+            (push (cadr (assq package package-archive-contents))
+                  upgrades)))))
+    (if upgrades
+        (when (or automatic
+                  (yes-or-no-p
+                   (message "Upgrade %d package%s (%s)? "
+                            (length upgrades)
+                            (if (= (length upgrades) 1) "" "s")
+                            (mapconcat #'package-desc-full-name upgrades ", "))))
+          (save-window-excursion
+            (dolist (package-desc upgrades)
+              (let ((old-package (cadr (assq (package-desc-name package-desc)
+                                             package-alist))))
+                (package-install package-desc)
+                (package-delete  old-package)))))
+      (message "All packages are up to date"))))
+
 (provide 'config-package)
