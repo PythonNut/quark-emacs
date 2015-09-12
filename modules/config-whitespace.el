0f6b6e47b0c01ac19e8e7fef4d912ed5234809c7
PythonNut <PythonNut@users.noreply.github.com>
2015-09-12 00:14:34 +0000

    Merge config-indent and config-whitespace

--- modules/config-whitespace.el
+++ modules/config-whitespace.el
@@ -4,9 +4,9 @@
   (with-demoted-errors "Load error: %s"
     (require 'auto-indent-mode)
     (require 'adaptive-wrap)
-    (require 'diminish)))
-
-(require 'config-indent)
+    (require 'diminish)
+    (require 'smie)
+    (require 'evil)))
 
 (setq require-final-newline t
       line-move-visual t)
@@ -43,10 +43,60 @@
   (remove-hook 'find-file-hook #'my/ws-butler-onetime-setup))
 
 (add-hook 'find-file-hook #'my/ws-butler-onetime-setup)
+(add-hook 'find-file-hook #'dtrt-indent-mode)
 
 ;; ws-butler also loads highlight-changes-mode
 (add-hook 'highlight-changes-mode-hook
           (lambda ()
             (diminish 'highlight-changes-mode)))
+
+(with-eval-after-load 'aggressive-indent
+  (diminish 'aggressive-indent-mode (if (display-graphic-p) " ⇶" " *→")))
+
+(with-eval-after-load 'auto-indent
+  (add-hook 'auto-indent-global-mode-hook
+            (lambda ()
+              (diminish 'auto-indent-mode (if (display-graphic-p) " ⇉" " →"))))
+
+  (add-hook 'auto-indent-mode-hook
+            (lambda ()
+              (diminish 'auto-indent-mode (if (display-graphic-p) " ⇉" " →")))))
+
+(defun my/auto-indent-onetime-setup ()
+  (auto-indent-global-mode +1)
+  (remove-hook 'first-change-hook
+               #'my/auto-indent-onetime-setup))
+
+(add-hook 'emacs-startup-hook
+          (lambda ()
+            (add-hook 'first-change-hook #'my/auto-indent-onetime-setup)))
+
+(defun my/smie-auto-guess ()
+  (when (featurep 'smie)
+    (unless (eq smie-grammar 'unset)
+      (smie-config-guess))))
+
+(add-hook 'after-change-major-mode-hook #'my/smie-auto-guess)
+
+(evil-define-command back-to-indentation-or-beginning ()
+  (if (= (point)
+         (save-excursion (back-to-indentation) (point)))
+      (beginning-of-line)
+    (unless (and (bolp)
+                 (bound-and-true-p multiple-cursors-mode))
+      (back-to-indentation))))
+
+(define-key evil-insert-state-map (kbd "C-a")
+  #'back-to-indentation-or-beginning)
+(define-key evil-motion-state-map (kbd "C-a")
+  #'back-to-indentation-or-beginning)
+
+(define-key evil-insert-state-map (kbd "<home>")
+  #'back-to-indentation-or-beginning)
+(define-key evil-motion-state-map (kbd "<home>")
+  #'back-to-indentation-or-beginning)
+
+(define-key evil-insert-state-map (kbd "C-e") #'end-of-visual-line)
+(define-key evil-insert-state-map (kbd "<end>") #'end-of-visual-line)
 
 (provide 'config-whitespace)
