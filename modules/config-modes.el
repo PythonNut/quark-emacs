e0c54dc5ff6fadafffd0cfab3ec6dab9e3978a54
PythonNut <PythonNut@users.noreply.github.com>
2015-09-14 05:25:43 +0000

    Fix c-like mode settings

--- modules/config-modes.el
+++ modules/config-modes.el
@@ -61,32 +61,37 @@
 (add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
 
 (with-eval-after-load 'cc-mode
+  (setq c-default-style "k&r")
+
+  ;; prefer C++1y
+  (add-hook 'c++-mode-hook
+            (lambda ()
+              (setq flycheck-clang-language-standard "c++1y")))
+
+  ;; prefer C11
+  (add-hook 'c-mode-hook
+            (lambda ()
+              (setq flycheck-clang-language-standard "c11")))
+
   (with-eval-after-load 'smartparens
-    (setq c-default-style "k&r")
+    (cl-macrolet
+        ((my/setup-cc-mode
+          (mode)
+          `(progn
+             (sp-local-pair ,mode "/*" "*/" :post-handlers
+                            '(:add
+                              ("* ||\n[i]" "RET")))
+             (sp-local-pair ,mode "{" nil :post-handlers
+                            '(:add
+                              ("||\n[i]" "RET")
+                              ("| " "SPC"))))))
 
-    ;; prefer C++1y
-    (add-hook 'c++-mode-hook
-              (lambda ()
-                (setq flycheck-clang-language-standard "c++1y")))
-
-    ;; prefer C11
-    (add-hook 'c-mode-hook
-              (lambda ()
-                (setq flycheck-clang-language-standard "c11")))
-
-    (let ((my-c-modes
-           '('c++-mode
-             'java-mode
-             'c-mode)))
-
-      (dolist (mode my-c-modes)
-        (sp-local-pair mode "/*" "*/" :post-handlers
-                       '(:add
-                         ("* ||\n[i]" "RET")))
-        (sp-local-pair mode "{" nil :post-handlers
-                       '(:add
-                         ("||\n[i]" "RET")
-                         ("| " "SPC")))))))
+      (with-no-warnings
+        (my/generate-calls-single
+         'my/setup-cc-mode
+         ('c++-mode
+          'java-mode
+          'c-mode))))))
 
 (package-deferred-install 'arduino-mode
     :mode-entries '('("\\.pde\\'" . arduino-mode)
