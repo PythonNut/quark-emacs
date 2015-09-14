5b9e37a1937ac3a3786141714163406334ddb116
PythonNut <PythonNut@users.noreply.github.com>
2015-09-14 04:45:41 +0000

    Change default TRAMP method to scp

--- modules/config-tramp.el
+++ modules/config-tramp.el
@@ -8,18 +8,16 @@
   ;; cache passwords for the duration of the session
   ;; note that said cache is _not_ persistent
   (setq password-cache-expiry nil
-        tramp-default-method "ssh"))
+        tramp-default-method "scp"))
 
 ;; =================================
 ;; automatically request root access
 ;; =================================
 (defun my/root-file-name-p (file-name)
-  (and
-   (featurep 'tramp)
-   (tramp-tramp-file-p file-name)
-   (with-parsed-tramp-file-name file-name parsed
-     (string= "root"
-              (substring-no-properties parsed-user)))))
+  (and (featurep 'tramp)
+       (tramp-tramp-file-p file-name)
+       (with-parsed-tramp-file-name file-name parsed
+         (string= "root" (substring-no-properties parsed-user)))))
 
 (defun my/make-root-file-name (file-name)
   (require 'tramp)
@@ -49,18 +47,16 @@
 (defun edit-file-as-root ()
   "Find file as root"
   (interactive)
-  (find-alternate-file
-   (my/make-root-file-name buffer-file-name)))
+  (find-alternate-file (my/make-root-file-name buffer-file-name)))
 
 (defun my/edit-file-as-root-maybe ()
   "Find file as root if necessary."
-  (when (and
-         buffer-file-name
-         (not (file-writable-p buffer-file-name))
-         (not (string= user-login-name
-                       (nth 3 (file-attributes buffer-file-name 'string))))
-         (not (my/root-file-name-p buffer-file-name))
-         (y-or-n-p "File is not writable. Open with root? "))
+  (when (and buffer-file-name
+             (not (file-writable-p buffer-file-name))
+             (not (string= user-login-name
+                           (nth 3 (file-attributes buffer-file-name 'string))))
+             (not (my/root-file-name-p buffer-file-name))
+             (y-or-n-p "File is not writable. Open with root? "))
     (edit-file-as-root)))
 
 (add-hook 'find-file-hook #'my/edit-file-as-root-maybe)
@@ -70,14 +66,12 @@
   (condition-case nil
       (apply old-fun buf filename args)
     (file-error
-     (if (and
-          (not (my/root-file-name-p filename))
-          (y-or-n-p "File is not readable. Open with root? "))
+     (if (and (not (my/root-file-name-p filename))
+              (y-or-n-p "File is not readable. Open with root? "))
          (let ((filename (my/make-root-file-name (file-truename filename))))
            (apply #'find-file-noselect-1
-                  (or
-                   (get-file-buffer filename)
-                   (create-file-buffer filename))
+                  (or (get-file-buffer filename)
+                      (create-file-buffer filename))
                   filename
                   args))
        (signal 'file-error (list "File is not readable" filename))))))
