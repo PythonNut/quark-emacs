5763200098145888713ef9929281ac6127fbf67e
PythonNut <PythonNut@users.noreply.github.com>
2015-10-01 05:07:02 +0000

    Use C-x C-r to rename the buffer and file at the same time

--- modules/config-ui.el
+++ modules/config-ui.el
@@ -343,4 +343,24 @@
 
 (define-key isearch-mode-map (kbd "<backspace>") #'isearch-delete-something)
 
+;; add intelligent buffer renaming
+(defun rename-current-buffer-file ()
+  "Renames current buffer and file it is visiting."
+  (interactive)
+  (let ((name (buffer-name))
+        (filename (buffer-file-name)))
+    (if (not (and filename (file-exists-p filename)))
+        (error "Buffer '%s' is not visiting a file!" name)
+      (let ((new-name (read-file-name "New name: " filename)))
+        (if (get-buffer new-name)
+            (error "A buffer named '%s' already exists!" new-name)
+          (rename-file filename new-name 1)
+          (rename-buffer new-name)
+          (set-visited-file-name new-name)
+          (set-buffer-modified-p nil)
+          (message "File '%s' successfully renamed to '%s'"
+                   name (file-name-nondirectory new-name)))))))
+
+(global-set-key (kbd "C-x C-r") 'rename-current-buffer-file)
+
 (provide 'config-ui)
