1c66484d0fcab3723658cc0422a4655b7297bdd3
PythonNut <PythonNut@users.noreply.github.com>
2015-10-12 17:27:04 +0000

    Add the ability to set X11 urgency hints

--- modules/config-setq.el
+++ modules/config-setq.el
@@ -183,4 +183,32 @@ the syntax class ')'."
 
 (advice-add 'show-paren-function :after #'nadvice/show-paren-function)
 
+;; let emacs blink when something interesting happens.
+;; in KDE this marks the active Emacs icon in the tray.
+(defun x-urgency-hint (frame arg &optional source)
+  "Set the x-urgency hint for the frame to arg:
+
+- If arg is nil, unset the urgency.
+- If arg is any other value, set the urgency.
+
+If you unset the urgency, you still have to visit the frame to make the urgency setting disappear (at least in KDE)."
+  (let* ((wm-hints (append (x-window-property
+                            "WM_HINTS" frame "WM_HINTS"
+                            source nil t) nil))
+         (flags (car wm-hints)))
+                                        ; (message flags)
+    (setcar wm-hints
+            (if arg
+                (logior flags #x00000100)
+              (logand flags #x1ffffeff)))
+    (x-change-window-property "WM_HINTS" wm-hints frame "WM_HINTS" 32 t)))
+
+(defun x-urgent (&optional arg)
+  "Mark the current emacs frame as requiring urgent attention.
+
+With a prefix argument which does not equal a boolean value of nil, remove the urgency flag (which might or might not change display, depending on the window manager)."
+  (interactive "P")
+  (let (frame (car (car (cdr (current-frame-configuration)))))
+    (x-urgency-hint frame (not arg))))
+
 (provide 'config-setq)
