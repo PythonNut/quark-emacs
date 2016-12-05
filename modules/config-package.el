393837cf4ab40b7fc09e5ca47b11d2381064bf92
PythonNut <PythonNut@users.noreply.github.com>
2016-12-05 05:18:45 +0000

    Organize config-package

--- modules/config-package.el
+++ modules/config-package.el
@@ -4,6 +4,124 @@
 (eval-when-compile
   (require 'config-setq))
 
+;; ==============================================================================
+;; Do horrifying things to make package-initialize faster =======================
+;; ==============================================================================
+
+(defvar my/package-cached-autoloads nil)
+(defvar my/package-cached-descriptors nil)
+(defvar my/package-cache-last-build-time nil)
+
+(defvar my/package-autoload-file (locate-user-emacs-file
+                                  "data/package-cache.el"))
+
+(defun my/package-rebuild-cache ()
+  (interactive)
+  (let ((autoloads (file-expand-wildcards
+                    (expand-file-name "*/*-autoloads.el"
+                                      package-user-dir)))
+        (pkg-descs))
+
+    (with-temp-file my/package-autoload-file
+      (dolist (pkg-dir (cl-remove-if-not
+                        #'file-directory-p
+                        (file-expand-wildcards
+                         (expand-file-name "*" package-user-dir))))
+        (let ((pkg-file (expand-file-name
+                         (package--description-file pkg-dir)
+                         pkg-dir)))
+          (when (file-exists-p pkg-file)
+            (with-temp-buffer
+              (insert-file-contents pkg-file)
+              (goto-char (point-min))
+              (push (cons pkg-dir (read (current-buffer))) pkg-descs)))))
+
+      (insert (format "(setq my/package-cached-descriptors '%S)"
+                      pkg-descs))
+
+      (dolist (file autoloads)
+        (insert-file-contents file)
+
+        ;; detect custom themes
+        (when (with-temp-buffer
+                (insert-file-contents file)
+                (search-forward "'custom-theme-load-path" nil t))
+          (when (boundp 'custom-theme-load-path)
+            (insert (format "(add-to-list 'custom-theme-load-path \"%s\")"
+                            (file-name-as-directory
+                             (file-name-directory file)))))))
+
+      (insert (format "(setq my/package-cached-autoloads '%S)"
+                      (mapcar #'file-name-sans-extension autoloads)))
+
+      (let ((mtime (nth 6 (file-attributes
+                           (expand-file-name package-user-dir)))))
+        (insert (format "(setq my/package-cache-last-build-time '%S)" mtime))))
+    (cl-letf ((load-path))
+      (load my/package-autoload-file))))
+
+
+(dolist (dir (file-expand-wildcards
+              (expand-file-name "*" package-user-dir)))
+  (when (file-directory-p dir)
+    (add-to-list 'load-path dir)))
+
+(unless (file-exists-p my/package-autoload-file)
+  (my/package-rebuild-cache))
+
+(load my/package-autoload-file)
+
+(unless (equal (nth 6 (file-attributes
+                       (expand-file-name
+                        package-user-dir)))
+               my/package-cache-last-build-time)
+  (my/package-rebuild-cache))
+
+(defun nadvice/package-initialize (old-fun &rest args)
+  (cl-letf* ((orig-load (symbol-function #'load))
+             ((symbol-function #'load)
+              (lambda (&rest args)
+                (cl-destructuring-bind
+                    (file &rest args_ignored)
+                    args
+                  (unless (member file my/package-cached-autoloads)
+                    (when (assq (intern file) package-alist)
+                      (message "Package autoload cache miss: %s" file)
+                      (my/package-rebuild-cache))
+                    (apply orig-load args))))))
+    (apply old-fun args)))
+
+(advice-add 'package-initialize :around #'nadvice/package-initialize)
+
+(defun nadvice/package-load-descriptor (old-fun pkg-dir)
+  "Load the description file in directory PKG-DIR."
+  (let ((cached-desc (assoc pkg-dir my/package-cached-descriptors)))
+    (if cached-desc
+        (let* ((pkg-file (expand-file-name
+                          (package--description-file pkg-dir)
+                          pkg-dir))
+               (signed-file (concat pkg-dir ".signed"))
+               (pkg-desc (or (ignore-errors
+                               (package-process-define-package
+                                (cdr cached-desc)))
+                             (package-process-define-package
+                              (cdr cached-desc) pkg-file))))
+          (setf (package-desc-dir pkg-desc) pkg-dir)
+          (when (file-exists-p signed-file)
+            (setf (package-desc-signed pkg-desc) t))
+          pkg-desc)
+      ;; certain directories are queried, although they do not contain packages
+      (unless (member (file-name-nondirectory pkg-dir)
+                      '("elpa" ".emacs.d" "archives" "gnupg"))
+        (message "Package descriptor cache miss: %s" pkg-dir))
+      (funcall old-fun pkg-dir))))
+
+(advice-add 'package-load-descriptor :around #'nadvice/package-load-descriptor)
+
+;; ==============================================================================
+;; Guarantee all packages are installed on start ================================
+;; ==============================================================================
+
 ;; Package archives
 (setq package-enable-at-startup nil
       package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
@@ -75,57 +193,7 @@
     xclip
     yasnippet))
 
-(defvar my/package-cached-autoloads nil)
-(defvar my/package-cached-descriptors nil)
-(defvar my/package-cache-last-build-time nil)
-
-(defvar my/package-autoload-file (locate-user-emacs-file
-                                  "data/package-cache.el"))
-
-(defun my/package-rebuild-cache ()
-  (interactive)
-  (let ((autoloads (file-expand-wildcards
-                    (expand-file-name "*/*-autoloads.el"
-                                      package-user-dir)))
-        (pkg-descs))
-
-    (with-temp-file my/package-autoload-file
-      (dolist (pkg-dir (cl-remove-if-not
-                        #'file-directory-p
-                        (file-expand-wildcards
-                         (expand-file-name "*" package-user-dir))))
-        (let ((pkg-file (expand-file-name
-                         (package--description-file pkg-dir)
-                         pkg-dir)))
-          (when (file-exists-p pkg-file)
-            (with-temp-buffer
-              (insert-file-contents pkg-file)
-              (goto-char (point-min))
-              (push (cons pkg-dir (read (current-buffer))) pkg-descs)))))
-
-      (insert (format "(setq my/package-cached-descriptors '%S)"
-                      pkg-descs))
-
-      (dolist (file autoloads)
-        (insert-file-contents file)
-
-        ;; detect custom themes
-        (when (with-temp-buffer
-                (insert-file-contents file)
-                (search-forward "'custom-theme-load-path" nil t))
-          (when (boundp 'custom-theme-load-path)
-            (insert (format "(add-to-list 'custom-theme-load-path \"%s\")"
-                            (file-name-as-directory
-                             (file-name-directory file)))))))
-
-      (insert (format "(setq my/package-cached-autoloads '%S)"
-                      (mapcar #'file-name-sans-extension autoloads)))
-
-      (let ((mtime (nth 6 (file-attributes
-                           (expand-file-name package-user-dir)))))
-        (insert (format "(setq my/package-cache-last-build-time '%S)" mtime))))
-    (cl-letf ((load-path))
-      (load my/package-autoload-file))))
+(package-initialize)
 
 (defun my/package-installed-p (pkg)
   (package-installed-p (if (consp pkg)
@@ -144,66 +212,6 @@
                 pkg))
     (package-install pkg)))
 
-(dolist (dir (file-expand-wildcards
-              (expand-file-name "*" package-user-dir)))
-  (when (file-directory-p dir)
-    (add-to-list 'load-path dir)))
-
-(unless (file-exists-p my/package-autoload-file)
-  (my/package-rebuild-cache))
-
-(load my/package-autoload-file)
-
-(unless (equal (nth 6 (file-attributes
-                       (expand-file-name
-                        package-user-dir)))
-               my/package-cache-last-build-time)
-  (my/package-rebuild-cache))
-
-(defun nadvice/package-initialize (old-fun &rest args)
-  (cl-letf* ((orig-load (symbol-function #'load))
-             ((symbol-function #'load)
-              (lambda (&rest args)
-                (cl-destructuring-bind
-                    (file &rest args_ignored)
-                    args
-                  (unless (member file my/package-cached-autoloads)
-                    (when (assq (intern file) package-alist)
-                      (message "Package autoload cache miss: %s" file)
-                      (my/package-rebuild-cache))
-                    (apply orig-load args))))))
-    (apply old-fun args)))
-
-(advice-add 'package-initialize :around #'nadvice/package-initialize)
-
-(defun nadvice/package-load-descriptor (old-fun pkg-dir)
-  "Load the description file in directory PKG-DIR."
-  (let ((cached-desc (assoc pkg-dir my/package-cached-descriptors)))
-    (if cached-desc
-        (let* ((pkg-file (expand-file-name
-                          (package--description-file pkg-dir)
-                          pkg-dir))
-               (signed-file (concat pkg-dir ".signed"))
-               (pkg-desc (or (ignore-errors
-                               (package-process-define-package
-                                (cdr cached-desc)))
-                             (package-process-define-package
-                              (cdr cached-desc) pkg-file))))
-          (setf (package-desc-dir pkg-desc) pkg-dir)
-          (when (file-exists-p signed-file)
-            (setf (package-desc-signed pkg-desc) t))
-          pkg-desc)
-      ;; certain directories are queried, although they do not contain packages
-      (unless (member (file-name-nondirectory pkg-dir)
-                      '("elpa" ".emacs.d" "archives" "gnupg"))
-        (message "Package descriptor cache miss: %s" pkg-dir))
-      (funcall old-fun pkg-dir))))
-
-(advice-add 'package-load-descriptor :around #'nadvice/package-load-descriptor)
-
-(package-initialize)
-
-;; Guarantee all packages are installed on start
 (defun my/has-package-not-installed (packages)
   (catch 'package-return
     (dolist (package packages)
@@ -223,6 +231,10 @@
       (package-initialize))))
 
 (my/ensure-packages-are-installed my/required-packages)
+
+;; ==============================================================================
+;; Require packages in the background after startup =============================
+;; ==============================================================================
 
 (eval-when-compile
   (with-demoted-errors "Load error: %s"
@@ -282,6 +294,10 @@
 
 (add-hook 'emacs-startup-hook #'idle-require-mode)
 
+;; ==============================================================================
+;; Package manipulation functions ===============================================
+;; ==============================================================================
+
 (defun package-upgrade-all (&optional automatic)
   "Upgrade all packages automatically without showing *Packages* buffer."
   (interactive)
@@ -327,6 +343,35 @@
              (my/x-urgent)))
        (message "All packages are up to date")))))
 
+(defun package-uninstall (package-name)
+  (interactive
+   (let ((dir (expand-file-name package-user-dir)))
+     (list (completing-read
+            "Uninstall package: "
+            (mapcar (lambda (package-dir)
+                      (replace-regexp-in-string
+                       (rx "-" (one-or-more (any num ".")))
+                       ""
+                       (file-relative-name package-dir dir)))
+                    (cl-remove-if-not
+                     (lambda (item)
+                       (and (file-directory-p item)
+                            (not (string-match-p (rx (or "archives" ".")
+                                                     line-end)
+                                                 item))))
+                     (directory-files dir t)))))))
+
+  (dolist (item (file-expand-wildcards
+                 (expand-file-name (concat package-name "*")
+                                   package-user-dir)))
+    (delete-directory item t))
+  (message "Successfully deleted package %s."
+           (substring-no-properties package-name)))
+
+;; ==============================================================================
+;; Deferred package installation macro ==========================================
+;; ==============================================================================
+
 (eval-and-compile
   (defun my/remove-keyword-params (seq)
     (when seq
@@ -366,32 +411,9 @@
            (list `(with-eval-after-load ,(or feature-name package-name)
                     ,@forms))))))
 
+;; Load this early, so we can potentially catch bugs
+;; early in the init process
 (package-deferred-install 'bug-hunter
     :autoload-names '('bug-hunter-file 'bug-hunter-init-file))
-
-(defun package-uninstall (package-name)
-  (interactive
-   (let ((dir (expand-file-name package-user-dir)))
-     (list (completing-read
-            "Uninstall package: "
-            (mapcar (lambda (package-dir)
-                      (replace-regexp-in-string
-                       (rx "-" (one-or-more (any num ".")))
-                       ""
-                       (file-relative-name package-dir dir)))
-                    (cl-remove-if-not
-                     (lambda (item)
-                       (and (file-directory-p item)
-                            (not (string-match-p (rx (or "archives" ".")
-                                                     line-end)
-                                                 item))))
-                     (directory-files dir t)))))))
-
-  (dolist (item (file-expand-wildcards
-                 (expand-file-name (concat package-name "*")
-                                   package-user-dir)))
-    (delete-directory item t))
-  (message "Successfully deleted package %s."
-           (substring-no-properties package-name)))
 
 (provide 'config-package)
