d452cf60da057aa1e12c4324b11c575f81dc3abe
PythonNut <PythonNut@users.noreply.github.com>
2016-12-05 06:43:21 +0000

    First try at adding R Markdown support

--- modules/config-modes.el
+++ modules/config-modes.el
@@ -1305,6 +1305,53 @@
                       'S))
 
 ;; =============================================================================
+;; Polymodes ===================================================================
+;; =============================================================================
+
+(package-deferred-install 'polymode
+    :autoload-names '('poly-markdown-mode
+                      'poly-noweb+r-mode
+                      'poly-noweb+r-mode
+                      'poly-markdown+r-mode
+                      'poly-rapport-mode
+                      'poly-html+r-mode
+                      'poly-brew+r-mode
+                      'poly-r+c++-mode
+                      'poly-c++r-mode
+                      'poly-javascript+erb-mode
+                      'poly-coffee+erb-mode
+                      'poly-html+erb-mode
+                      'poly-slim-mode))
+
+(with-eval-after-load 'polymode
+  (require 'poly-R)
+  (require 'poly-erb)
+  (require 'poly-markdown)
+  (require 'poly-noweb)
+  (require 'poly-slim))
+
+;;; MARKDOWN
+(add-to-list 'auto-mode-alist '("\\.md$" . poly-markdown-mode))
+
+;;; R related modes
+(add-to-list 'auto-mode-alist '("\\.Snw$" . poly-noweb+r-mode))
+(add-to-list 'auto-mode-alist '("\\.Rnw$" . poly-noweb+r-mode))
+(add-to-list 'auto-mode-alist '("\\.Rmd$" . poly-markdown+r-mode))
+(add-to-list 'auto-mode-alist '("\\.rapport$" . poly-rapport-mode))
+(add-to-list 'auto-mode-alist '("\\.Rhtml$" . poly-html+r-mode))
+(add-to-list 'auto-mode-alist '("\\.Rbrew$" . poly-brew+r-mode))
+(add-to-list 'auto-mode-alist '("\\.Rcpp$" . poly-r+c++-mode))
+(add-to-list 'auto-mode-alist '("\\.cppR$" . poly-c++r-mode))
+
+;;; ERB modes
+(add-to-list 'auto-mode-alist '("\\.js.erb$" . poly-javascript+erb-mode))
+(add-to-list 'auto-mode-alist '("\\.coffee.erb$" . poly-coffee+erb-mode))
+(add-to-list 'auto-mode-alist '("\\.html.erb$" . poly-html+erb-mode))
+
+;;; Slim mode
+(add-to-list 'auto-mode-alist '("\\.slim$" . poly-slim-mode))
+
+;; =============================================================================
 ;; Speculative languages =======================================================
 ;; =============================================================================
 
