681a6e44cfb7962b80fbdc962252b5ab93608c93
PythonNut <PythonNut@users.noreply.github.com>
2018-01-13 22:57:09 -0800

    Cleanup motion assignment in config-avy-easymotion

--- modules/config-avy-easymotion.el
+++ modules/config-avy-easymotion.el
@@ -1,22 +1,24 @@
 ;; -*- lexical-binding: t -*-
 
-(defun nadvice/self-insert-command (old-fun &optional arg)
-  (interactive "P")
-  (cond
-   ;; `C-u a` jumps to `a`.
-   ((consp arg)
-    (avy-goto-char last-command-event))
-
-   ;; `C-- a` jumps to `a` at the beginning of a (sub)word
-   ((eq '- arg)
-    (avy-goto-subword-1 last-command-event))
-
-   ((or (numberp arg) (not arg))
-    (funcall old-fun (or arg 1)))))
-
-(advice-add 'self-insert-command :around #'nadvice/self-insert-command)
-
 (use-package avy
+  :init
+  (defun nadvice/self-insert-command (old-fun &optional arg)
+    (interactive "P")
+    (cond
+     ;; `C-u a` jumps to `a`.
+     ((consp arg)
+      (avy-goto-char last-command-event))
+
+     ;; `C-- a` jumps to `a` at the beginning of a (sub)word
+     ((eq '- arg)
+      (avy-goto-subword-1 last-command-event))
+
+     ((or (numberp arg) (not arg))
+      (funcall old-fun (or arg 1)))))
+
+  (advice-add 'self-insert-command :around #'nadvice/self-insert-command)
+
+  (global-set-key (kbd "<remap> <goto-line>") #'evil-avy-goto-line)
   :config
   (eval-when-compile
     (with-demoted-errors "Load error: %s"
@@ -62,41 +64,35 @@
 
     (key-chord-define evil-emacs-state-map "jk" #'avy-goto-char-timer)
     (key-chord-define evil-emacs-state-map "jw" #'evil-avy-goto-word-1)
-    (key-chord-define evil-emacs-state-map "jl" #'evil-avy-goto-line)
-
-    (define-key evil-normal-state-map (kbd "SPC l") #'evil-avy-goto-line)
-    (define-key evil-motion-state-map (kbd "SPC l") #'evil-avy-goto-line)
-    (define-key evil-normal-state-map (kbd "SPC c") #'avy-goto-char-timer)
-    (define-key evil-motion-state-map (kbd "SPC c") #'avy-goto-char-timer)))
-
-(global-set-key (kbd "<remap> <goto-line>") #'evil-avy-goto-line)
+    (key-chord-define evil-emacs-state-map "jl" #'evil-avy-goto-line)))
 
 (use-package ace-window
+  :init
+  ;; bind command to switch to minibuffer
+  (defun switch-window-dwim (arg)
+    "switch to minibuffer window (if active)"
+    (interactive "P")
+    (let ((num-windows (length (mapcar #'window-buffer (window-list)))))
+      (cond ((= num-windows 1)
+             (call-interactively #'split-window-right))
+            ((minibufferp)
+             (other-window (or arg 1)))
+            ((active-minibuffer-window)
+             (select-window (active-minibuffer-window)))
+            ((or (> (length (visible-frame-list)) 1)
+                 (> num-windows 3)
+                 (numberp arg))
+             (ace-window arg))
+            (t
+             (other-window (or arg 1))))))
+
+  (global-set-key (kbd "C-'") #'switch-window-dwim)
+  (global-set-key (kbd "C-c '") #'switch-window-dwim)
+
   :config
   (setq aw-keys (eval-when-compile (string-to-list "jfkdlsautnvmircieowpq"))
         aw-ignore-current t
         aw-swap-invert t))
-
-;; bind command to switch to minibuffer
-(defun switch-window-dwim (arg)
-  "switch to minibuffer window (if active)"
-  (interactive "P")
-  (let ((num-windows (length (mapcar #'window-buffer (window-list)))))
-    (cond ((= num-windows 1)
-           (call-interactively #'split-window-right))
-          ((minibufferp)
-           (other-window (or arg 1)))
-          ((active-minibuffer-window)
-           (select-window (active-minibuffer-window)))
-          ((or (> (length (visible-frame-list)) 1)
-               (> num-windows 3)
-               (numberp arg))
-           (ace-window arg))
-          (t
-           (other-window (or arg 1))))))
-
-(global-set-key (kbd "C-'") #'switch-window-dwim)
-(global-set-key (kbd "C-c '") #'switch-window-dwim)
 
 (use-package evil-easymotion
   :init
@@ -142,34 +138,36 @@
   (define-key evilem-map "-" #'evilem-motion-previous-line-first-non-blank)
   (define-key evilem-map "+" #'evilem-motion-next-line-first-non-blank)
 
+  (define-key evilem-map (kbd "g s f") (evilem-create #'on-parens-forward-sexp-end))
+  (define-key evilem-map (kbd "g s b") (evilem-create #'on-parens-backward-sexp))
+  (define-key evilem-map (kbd "g s d") (evilem-create #'on-parens-down-sexp))
+  (define-key evilem-map (kbd "g s D") (evilem-create #'on-parens-down-sexp-end))
+  (define-key evilem-map (kbd "g s u") (evilem-create #'on-parens-up-sexp-end))
+  (define-key evilem-map (kbd "g s U") (evilem-create #'on-parens-up-sexp))
+  (define-key evilem-map (kbd "g s n") (evilem-create #'on-parens-forward-sexp))
+  (define-key evilem-map (kbd "g s p") (evilem-create #'on-parens-backward-sexp-end))
+
+  (define-key evilem-map (kbd "s")
+    (evilem-create #'evil-snipe-repeat
+                   :pre-hook (save-excursion
+                               (ignore-errors
+                                 (call-interactively #'evil-snipe-s)))
+                   :bind ((evil-snipe-enable-highlight)
+                          (evil-snipe-enable-incremental-highlight))))
+
+  (define-key evilem-map (kbd "S")
+    (evilem-create #'evil-snipe-repeat-reverse
+                   :pre-hook (save-excursion
+                               (ignore-errors
+                                 (call-interactively #'evil-snipe-S)))
+                   :bind ((evil-snipe-enable-highlight)
+                          (evil-snipe-enable-incremental-highlight))))
+
+  ;; These are easymotions, but whatever....
+  (define-key evilem-map (kbd "l") #'evil-avy-goto-line)
+  (define-key evilem-map (kbd "c") #'avy-goto-char-timer)
+
   (evilem-default-keybindings "SPC")
-
-  (use-package on-parens
-    ;; Note: on-parens is ensured in config-smartparens
-    :ensure nil
-    :config
-    (evilem-define (kbd "SPC g s f") #'on-parens-forward-sexp-end)
-    (evilem-define (kbd "SPC g s b") #'on-parens-backward-sexp)
-    (evilem-define (kbd "SPC g s d") #'on-parens-down-sexp)
-    (evilem-define (kbd "SPC g s D") #'on-parens-down-sexp-end)
-    (evilem-define (kbd "SPC g s u") #'on-parens-up-sexp-end)
-    (evilem-define (kbd "SPC g s U") #'on-parens-up-sexp)
-    (evilem-define (kbd "SPC g s n") #'on-parens-forward-sexp)
-    (evilem-define (kbd "SPC g s p") #'on-parens-backward-sexp-end))
-
-  (evilem-define (kbd "SPC s") #'evil-snipe-repeat
-                 :pre-hook (save-excursion
-                             (ignore-errors
-                               (call-interactively #'evil-snipe-s)))
-                 :bind ((evil-snipe-enable-highlight)
-                        (evil-snipe-enable-incremental-highlight)))
-
-  (evilem-define (kbd "SPC S") #'evil-snipe-repeat-reverse
-                 :pre-hook (save-excursion
-                             (ignore-errors
-                               (call-interactively #'evil-snipe-S)))
-                 :bind ((evil-snipe-enable-highlight)
-                        (evil-snipe-enable-incremental-highlight)))
 
   (use-package evil-snipe
     ;; Note: evil-snipe is ensured in config-evil-modules
