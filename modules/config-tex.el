b39291581f142621e0be0b5799c15b3309622096
PythonNut <PythonNut@users.noreply.github.com>
2018-04-28 18:32:30 -0700

    Switch to working AUCTeX fork

--- modules/config-tex.el
+++ modules/config-tex.el
@@ -9,7 +9,9 @@
              company-math-symbols-unicode))
 
 (use-package tex
-  :recipe auctex
+  :recipe (auctex :host github :repo "raxod502/auctex"
+                  :branch "fork/1"
+                  :files (:defaults (:exclude "doc/*.texi")))
   :commands (bib-cite-minor-mode
              turn-on-bib-cite
              ConTeXt-mode
