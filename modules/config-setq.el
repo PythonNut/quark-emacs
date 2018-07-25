067cdca144f5840f2e83bdf065ab108c85c7a3e0
PythonNut <PythonNut@users.noreply.github.com>
2018-07-25 10:29:54 -0700

    Disable lockfiles

--- modules/config-setq.el
+++ modules/config-setq.el
@@ -3,6 +3,7 @@
 
 (setq ad-redefinition-action 'accept
       cursor-type 'box
+      create-lockfiles nil
       custom-safe-themes
       '("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879"
         "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4"
