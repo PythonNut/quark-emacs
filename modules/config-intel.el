20216701171306e5a44998fddf719c9105aaa29f
PythonNut <PythonNut@users.noreply.github.com>
2019-07-30 16:49:02 -0700

    Add command to print buffer with syntax highlighting

--- modules/config-intel.el
+++ modules/config-intel.el
@@ -562,4 +562,22 @@ is binary, activate `hexl-mode'."
     (cond ((executable-find "nautilus")
            (call-process "nautilus" nil nil nil dir)))))
 
+(defun pdf-print-buffer-with-faces (&optional filename)
+  "Print file in the current buffer as pdf, including font, color, and
+underline information.  This command works only if you are using a window system,
+so it has a way to determine color values.
+
+C-u COMMAND prompts user where to save the Postscript file (which is then
+converted to PDF at the same location."
+  (require 'ps-print)
+  (interactive (list (if current-prefix-arg
+                         (ps-print-preprint 4)
+                       (concat (file-name-sans-extension (buffer-file-name))
+                               ".ps"))))
+  (ps-print-with-faces (point-min) (point-max) filename)
+  (shell-command (concat "ps2pdf " filename))
+  (delete-file filename)
+  (message "Deleted %s" filename)
+  (message "Wrote %s" (concat (file-name-sans-extension filename) ".pdf")))
+
 (provide 'config-intel)
