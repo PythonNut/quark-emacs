f6c2186576062b1ee4b2ee6c02305de984f6a07b
PythonNut <PythonNut@users.noreply.github.com>
2020-10-06 22:33:01 -0700

    Remove cua-mode

--- modules/config-paste.el
+++ modules/config-paste.el
@@ -104,68 +104,17 @@ move the yanking point; just return the Nth kill forward."
      (smartparens-mode -1))))
 
 (use-package whole-line-or-region
-  :commands (whole-line-or-region-call-with-region
-             whole-line-or-region-call-with-prefix))
-
-(use-package cua-base
-  :ensure nil
   :init
-  (setq kill-do-not-save-duplicates t
-        cua-paste-pop-rotate-temporarily t
-        cua-enable-cua-keys t
-        cua-virtual-rectangle-edges t
-        cua-auto-tabify-rectangles nil
-        cua-rectangle-mark-key (kbd "C-x SPC"))
-  (cua-mode +1)
-  :config
-  (evil-define-key 'normal cua-global-keymap (kbd "C-v") nil))
-
-;; cua-cut the line if no region
-(advice-add
- 'cua-cut-region :around
- (my/defun-as-value nadvice/cua-cut-region (old-fun &optional prefix)
-   (interactive "*p")
-   (whole-line-or-region-wrap-region-kill
-    (lambda (_beg _end &optional _region)
-      (interactive (list (mark) (point) 'region))
-      (funcall old-fun current-prefix-arg))
-    0)))
-
-;; cua-yank a line if cut as a line
-(advice-add
- 'cua-paste :around
- (my/defun-as-value nadvice/cua-paste (old-fun raw-prefix)
-   ;; figure out what yank would do normally
-   (let ((string-to-yank (current-kill
-                          (cond ((listp raw-prefix) 0)
-                                ((eq raw-prefix '-) -1)
-                                (t (1- raw-prefix))) t))
-         (saved-column (current-column)))
-
-     ;; check for whole-line prop in yanked text
-     (if (get-text-property 0 'whole-line-or-region string-to-yank)
-         (save-excursion
-           (end-of-line)
-           (insert "\n")
-           (let ((beg (line-beginning-position)))
-             (insert (string-remove-suffix "\n" string-to-yank))
-             (remove-text-properties beg (1+ beg) '(whole-line-or-region nil))))
-
-       ;; no whole-line-or-region mark
-       (if (eq (car (get-text-property 0 'yank-handler
-                                       string-to-yank))
-               'evil-yank-line-handler)
-           (evil-paste-after raw-prefix)
-         (funcall old-fun raw-prefix))))))
-
-(defun easy-kill-on-my-line (_n)
-  "Get current line, but mark as a whole line for whole-line-or-region"
-  (let ((str (thing-at-point 'line)))
-    (save-excursion
-      (put-text-property 0 1 'whole-line-or-region t str)
-      (easy-kill-adjust-candidate 'my-line str))))
+  (define-key evil-emacs-state-map (kbd "C-w") nil)
+  (define-key evil-insert-state-map (kbd "C-w") nil)
+  (global-set-key (kbd "<remap> <kill-region>")
+                  #'whole-line-or-region-kill-region))
 
 (use-package easy-kill
+  :init
+  (global-set-key (kbd "<remap> <kill-ring-save>")
+                  #'easy-kill)
+
   :config
   (setq easy-kill-try-things '(url email my-line)))
 
@@ -288,17 +237,6 @@ move the yanking point; just return the Nth kill forward."
     (with-demoted-errors "Load error: %s"
       (require 'evil)))
 
-  (define-key evil-insert-state-map (kbd "C-x SPC") #'cua-set-rectangle-mark)
-  (define-key evil-emacs-state-map (kbd "C-x SPC") #'cua-set-rectangle-mark)
-  (define-key evil-insert-state-map (kbd "C-v") #'cua-paste)
-
-  (put 'evil-forward-char             'CUA 'move)
-  (put 'evil-backward-char            'CUA 'move)
-  (put 'evil-next-visual-line         'CUA 'move)
-  (put 'evil-previous-visual-line     'CUA 'move)
-  (put 'evil-end-of-visual-line       'CUA 'move)
-  (put 'evil-beginning-of-visual-line 'CUA 'move)
-
   ;; make evil respect whole-line-or-region
   (defun nadvice/evil-paste-line (&rest _args)
     (when (with-demoted-errors "Failed to check text properties for paste. %s"
@@ -309,64 +247,8 @@ move the yanking point; just return the Nth kill forward."
   (advice-add 'evil-paste-before :before #'nadvice/evil-paste-line)
   (advice-add 'evil-paste-after  :before #'nadvice/evil-paste-line)
 
-
-  ;; unify evil-paste with cua rectangles
-  (advice-add
-   'evil-paste-after :around
-   (my/defun-as-value nadvice/evil-paste-after (old-fun &rest args)
-     (if (eq (with-demoted-errors "Failed to check text properties for paste. %s"
-               (car (get-text-property 0 'yank-handler (car kill-ring))))
-             'rectangle--insert-for-yank)
-         (evil-with-state 'normal
-           (call-interactively #'evil-append)
-           (call-interactively #'cua-paste))
-       (apply old-fun args))))
-
-  (advice-add
-   'evil-paste-before :around
-   (my/defun-as-value nadvice/evil-paste-before (old-fun &rest args)
-     (if (eq (with-demoted-errors "Failed to check text properties for paste. %s"
-               (car (get-text-property 0 'yank-handler (car kill-ring))))
-             'rectangle--insert-for-yank)
-         (evil-with-state 'normal
-           (call-interactively #'evil-insert)
-           (call-interactively #'cua-paste))
-       (apply old-fun args))))
-
-  (define-key evil-insert-state-map (kbd "C-w") nil)
-
-
-  (define-key evil-insert-state-map
-    (kbd "<remap> <kill-ring-save>") #'easy-kill)
-  (define-key evil-emacs-state-map
-    (kbd "<remap> <kill-ring-save>") #'easy-kill)
-  (define-key evil-normal-state-map
-    (kbd "<remap> <kill-ring-save>") #'easy-kill)
-
-  (define-key evil-emacs-state-map
-    (kbd "<remap> <kill-region>") #'cua-cut-region)
-  (define-key evil-insert-state-map
-    (kbd "<remap> <kill-region>") #'cua-cut-region)
-
-  (define-key evil-emacs-state-map
-    (kbd "<remap> <yank-pop>") #'cua-paste-pop)
-  (define-key evil-insert-state-map
-    (kbd "<remap> <yank-pop>") #'cua-paste-pop)
-
-  (define-key evil-emacs-state-map (kbd "C-y") #'cua-paste)
-  (define-key evil-insert-state-map (kbd "C-y") #'cua-paste)
-
-  ;; ensure cua-mode doesn't interfere with evil visual state
-  (let ((my/cua-mode-was-on))
-    (add-hook 'evil-visual-state-entry-hook
-              (my/defun-as-value my/entering-visual-state-disable-cua ()
-                (setq my/cua-mode-was-on cua-mode)
-                (when cua-mode
-                  (cua-mode -1))))
-    (add-hook 'evil-visual-state-exit-hook
-              (my/defun-as-value my/leaving-visual-state-enable-cua ()
-                (when my/cua-mode-was-on
-                  (cua-mode +1))))))
+  (define-key evil-emacs-state-map (kbd "C-y") nil)
+  (define-key evil-insert-state-map (kbd "C-y") nil))
 
 (use-package legalese
   :defer-install t
@@ -376,66 +258,7 @@ move the yanking point; just return the Nth kill forward."
   "A quick command to drop clipboard formatting"
   (interactive)
   (with-temp-buffer
-    (cua-paste nil)
-    (push-mark (point))
-    (push-mark (point-max) nil t)
-    (goto-char (point-min))
-    (cua-cut-region nil)))
-
-(el-patch-evil-define-command evil-visual-paste (count &optional register)
-  "Paste over Visual selection."
-  :suppress-operator t
-  (interactive "*P<x>")
-  (setq count (prefix-numeric-value count))
-  ;; evil-visual-paste is typically called from evil-paste-before or
-  ;; evil-paste-after, but we have to mark that the paste was from
-  ;; visual state
-  (setq this-command 'evil-visual-paste)
-  (el-patch-let (($paste (if paste-eob
-                             (evil-paste-after count register)
-                           (evil-paste-before count register))))
-    (let* ((text (if register
-                     (evil-get-register register)
-                   (current-kill 0)))
-           (yank-handler (car-safe (get-text-property
-                                    0 'yank-handler text)))
-           new-kill
-           paste-eob)
-      (evil-with-undo
-        (let* (el-patch-swap
-                ((kill-ring (list (current-kill 0)))
-                 (kill-ring-yank-pointer kill-ring))
-                ((kill-ring kill-ring)
-                 (kill-ring-yank-pointer kill-ring-yank-pointer)
-                 (kill-ring-yank-index kill-ring-yank-index)))
-          (when (evil-visual-state-p)
-            (evil-visual-rotate 'upper-left)
-            ;; if we replace the last buffer line that does not end in a
-            ;; newline, we use `evil-paste-after' because `evil-delete'
-            ;; will move point to the line above
-            (when (and (= evil-visual-end (point-max))
-                       (/= (char-before (point-max)) ?\n))
-              (setq paste-eob t))
-            (evil-delete evil-visual-beginning evil-visual-end
-                         (evil-visual-type))
-            (when (and (eq yank-handler #'evil-yank-line-handler)
-                       (not (eq (evil-visual-type) 'line))
-                       (not (= evil-visual-end (point-max))))
-              (insert "\n"))
-            (evil-normal-state)
-            (setq new-kill (current-kill 0))
-            (current-kill 1))
-          (el-patch-remove $paste))
-        (el-patch-add $paste)
-        (when evil-kill-on-visual-paste
-          (kill-new new-kill))
-        ;; mark the last paste as visual-paste
-        (setq evil-last-paste
-              (list (nth 0 evil-last-paste)
-                    (nth 1 evil-last-paste)
-                    (nth 2 evil-last-paste)
-                    (nth 3 evil-last-paste)
-                    (nth 4 evil-last-paste)
-                    t))))))
+    (yank nil)
+    (kill-region (point-min) (point-max))))
 
 (provide 'config-paste)
