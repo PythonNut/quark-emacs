3b80f37cdb34b45e5c14abf63c0413793a41224c
PythonNut <PythonNut@users.noreply.github.com>
2021-03-13 21:22:04 -0800

    Enable company-box

--- modules/config-company.el
+++ modules/config-company.el
@@ -42,6 +42,13 @@
             ('text-mode-hook       '(company-ispell))
             ('go-mode-hook         '(company-go)))))))
 
+  (use-package company-box
+    :init
+    (add-hook 'company-mode-hook #'company-box-mode)
+    :config
+    (diminish 'company-box-mode)
+    (setq company-box-enable-icon nil))
+
   :config
   (diminish 'company-mode (if (display-graphic-p) " ❃" " *"))
   (company-flx-mode +1)
