4c2e39f4b7b2c545e55aaced7059af4636121d0e
PythonNut <PythonNut@users.noreply.github.com>
2023-06-13 21:43:06 +0000

    straight: update to boostrap version 6

--- modules/config-package.el
+++ modules/config-package.el
@@ -14,18 +14,17 @@
         straight-recipes-gnu-elpa-use-mirror t)
 
   (defvar bootstrap-version)
-  (let ((bootstrap-file
-         (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
-        (bootstrap-version 5))
-    (unless (file-exists-p bootstrap-file)
-      (with-current-buffer
-          (url-retrieve-synchronously
-           "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
-           'silent 'inhibit-cookies)
-        (goto-char (point-max))
-        (eval-print-last-sexp)))
-    (unless (featurep 'straight)
-      (load bootstrap-file nil 'nomessage))))
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 6))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage)))
 
 ;; =============================================
 ;; Setup use-package
