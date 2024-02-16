d5eef2e81ace620f6c6ebc957b17d96f26893519
PythonNut <PythonNut@users.noreply.github.com>
2024-02-15 23:06:12 -0800

    Update straight bootstrap to version 7

--- modules/config-package.el
+++ modules/config-package.el
@@ -8,23 +8,26 @@
 
 (eval-and-compile
   (setq straight-repository-branch "develop"
-        straight-check-for-modifications 'live
+        straight-check-for-modifications '(check-on-save)
         straight-use-package-version 'ensure
         straight-use-package-by-default t
         straight-recipes-gnu-elpa-use-mirror t)
 
   (defvar bootstrap-version)
-(let ((bootstrap-file
-       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
-      (bootstrap-version 6))
-  (unless (file-exists-p bootstrap-file)
-    (with-current-buffer
-        (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
-         'silent 'inhibit-cookies)
-      (goto-char (point-max))
-      (eval-print-last-sexp)))
-  (load bootstrap-file nil 'nomessage)))
+  (let ((bootstrap-file
+         (expand-file-name
+          "straight/repos/straight.el/bootstrap.el"
+          (or (bound-and-true-p straight-base-dir)
+              user-emacs-directory)))
+        (bootstrap-version 7))
+    (unless (file-exists-p bootstrap-file)
+      (with-current-buffer
+          (url-retrieve-synchronously
+           "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+           'silent 'inhibit-cookies)
+        (goto-char (point-max))
+        (eval-print-last-sexp)))
+    (load bootstrap-file nil 'nomessage)))
 
 ;; =============================================
 ;; Setup use-package
